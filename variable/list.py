97a1b020e2525e88a3ded9db6c7720aa427490c4
Slavoljub Popovic <slavo@mimicom24.com>
2017-10-04 23:39:56 -0400

    list

--- variable/list.py
+++ variable/list.py
@@ -2,5 +2,5 @@
 print ("************* list ***************************")
 my_list = ['Slavo', 36, "Python", "Swift", 3450]
 print (my_list)
-print ("*************  acces to 2nd element in the list ***************************")
+print ("*************  acces to 3nd element in the list ***************************")
 print (my_list[2])
