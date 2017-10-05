f0a61186defdc2d895437643e4060a235e72ba83
slavo@mimicom24.com <slavo@mimicom24.com>
2017-10-05 19:56:02 -0400

    coding ex

--- coding_exercise/exercise1.py
+++ coding_exercise/exercise1.py
@@ -5,3 +5,13 @@ def temperatura (celsius):
     return fahrenheit
 
 print("Celsius: 36,6 Fahremheit: ", temperatura(36.6))
+
+# The lowest possible temperature that physical matter can reach is -273.15 °C. With that in mind, please improve the function by making it print out a message in case a number lower than -273.15 is passed as input when calling the function.
+
+def temp (c):    
+    if c < -273.15:        
+        return "That temperature doesn't make sense!"    
+    else:        
+        f = c * 9/5 + 32        
+        return f
+print(temp(-273.4))
