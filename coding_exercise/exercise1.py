6f640e9004dbf9324535e9250c11e46ff8b33555
slavo@mimicom24.com <slavo@mimicom24.com>
2017-10-05 19:40:45 -0400

    adding function

--- coding_exercise/exercise1.py
+++ coding_exercise/exercise1.py
@@ -1,2 +1,7 @@
 # Please create a function that converts Celsius degrees to Fahrenheit. The formula to convert   Celsius to Fahrenheit is F = C × 9/5 + 32.
 
+def temperatura (celsius):
+    fahrenheit = celsius * 9/5 + 32
+    return fahrenheit
+
+print("Celsius: 36,6 Fahremheit: ", temperatura(36.6))
