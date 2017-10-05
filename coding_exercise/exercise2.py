71c1791039b341a5700165b76a0eba71e95a2c0a
slavo@mimicom24.com <slavo@mimicom24.com>
2017-10-05 19:46:27 -0400

    printing dic

--- coding_exercise/exercise2.py
+++ coding_exercise/exercise2.py
@@ -2,4 +2,4 @@
 # money={"saving_account":200, "checking_account":100, "under_bed":[500,10,100]}
 
 money={"saving_account":200, "checking_account":100, "under_bed":[500,10,100]}
-print(money.under_bed[1])
+print(money['under_bed'][1])
