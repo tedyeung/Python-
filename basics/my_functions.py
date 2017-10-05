1f0a4dd9fda1b1cf92e50fec479f25725c52419c
slavo@mimicom24.com <slavo@mimicom24.com>
2017-10-05 19:10:42 -0400

    constom function

--- basics/my_functions.py
+++ basics/my_functions.py
@@ -2,3 +2,11 @@
 def minutes_to_hours (minutes):
     hours = minutes / 60 
     return hours 
+
+print (minutes_to_hours(60))
+
+def seconds_to_hours (sec):
+    hours = sec / 3600
+    return hours
+
+print (seconds_to_hours(56000))
