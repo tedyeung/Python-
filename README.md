c98720095377d364d11ab20512a0e094f9568fda
slavo@mimicom24.com <slavo@mimicom24.com>
2017-10-05 19:51:00 -0400

    higher

--- README.md
+++ README.md
@@ -78,3 +78,6 @@
 
 ## Errors 
 ### Sytax Error and excepcion
+
+# Conditionals
+### = , >, <, <= ,>=
---
+++ basics/conditionals.py
@@ -0,0 +1,14 @@
+# conditional 
+a = 5 
+if a < 5:
+    print("less then 5")
+else: 
+    print("equal to 5")
+# conditional el if  
+
+if a < 5:
+    print("less then 5")
+elif a == 5:
+    print("equal to 5")
+else: 
+    print("higher then 5")
