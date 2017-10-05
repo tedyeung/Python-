de8b9fb2b0b8787cd9a3cb1bcef4d12a5d3a548d
slavo@mimicom24.com <slavo@mimicom24.com>
2017-10-05 19:43:33 -0400

    new coding ext

---
+++ coding_exercise/exercise2.py
@@ -0,0 +1,5 @@
+# Try to print out number 10 only from the following dictionary.
+# money={"saving_account":200, "checking_account":100, "under_bed":[500,10,100]}
+
+money={"saving_account":200, "checking_account":100, "under_bed":[500,10,100]}
+print(money.under_bed[1])
