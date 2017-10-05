4f7470565ef35fe8f0222d381143396c0238abe5
Slavoljub Popovic <slavo@mimicom24.com>
2017-10-04 23:01:02 -0400

    type

--- Var, Data Types, Func/variables.py
+++ Var, Data Types, Func/variables.py
@@ -1,3 +1,10 @@
-# simple varibale 
+# simple string variable
+print("******************* Variables ****************************")
 name = "Slavo" # string varibale
 print(name)
+print(type(name))
+print("******************* numbers ****************************")
+a = 2 
+b = 3
+print (a , b)
+print (type(a))
