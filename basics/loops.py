f2f2fd29d98192f54e7aa2080b6743c3fbce4e42
slavo@mimicom24.com <slavo@mimicom24.com>
2017-10-05 21:00:26 -0400

    for loops

--- basics/loops.py
+++ basics/loops.py
@@ -1,4 +1,4 @@
 # for loop 
 emails = ['slavo@mimicom24.com', 'popovic.slavoljub@gmail.com', 'slavo@slavo7.com', 'slavoljub.popovic@yahoo.com', 'office@mimicom24.com']
 for item in emails:
-    print(emails[item])
+    print(item)
