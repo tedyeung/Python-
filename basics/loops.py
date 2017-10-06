c7db5a8eeb85feab2bec1ac81dfa6b25466bda92
slavo@mimicom24.com <slavo@mimicom24.com>
2017-10-05 21:01:38 -0400

    print item

--- basics/loops.py
+++ basics/loops.py
@@ -2,3 +2,7 @@
 emails = ['slavo@mimicom24.com', 'popovic.slavoljub@gmail.com', 'slavo@slavo7.com', 'slavoljub.popovic@yahoo.com', 'office@mimicom24.com']
 for item in emails:
     print(item)
+
+for item in emails:
+    if 'gmail' in item:
+        print(item)
