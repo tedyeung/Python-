f6f3e6beff108325484e617c076b3a1ea4a5c6b7
slavo@mimicom24.com <slavo@mimicom24.com>
2017-10-08 22:11:18 -0400

    write small program

--- file_hadling/opening_files.py
+++ file_hadling/opening_files.py
@@ -2,5 +2,12 @@
 file_obj = open("example.txt")
 content = file_obj.read()
 print(content)
+file_obj.close()
 # write in to the file
+first_python_edit = open('new_example.txt', 'w')
+first_python_edit.write('Welcome Slavo, You wrote your first line')
+first_python_edit.close()
+print(first_python_edit.read())
+
+
 
