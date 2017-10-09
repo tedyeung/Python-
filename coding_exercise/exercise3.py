00a35772775fd1c81e7c32beeb4a1e8a3b4609cb
slavo@mimicom24.com <slavo@mimicom24.com>
2017-10-08 22:47:57 -0400

    exercise files

---
+++ coding_exercise/exercise3.py
@@ -0,0 +1,23 @@
+# In exercise 4, you recursively printed out converted temperature in the command line. For this exercise, please consider the same list of Celsius values again as input:
+
+# temperatures=[10,-20,-289,100]
+
+# Try to make a script that converts Celsius to Fahrenheit and creates a text file and stores the converted values inside the text file. Your text file content should look like this:
+
+ # 50.0
+ # -4.0
+ # 212.0
+
+# Please don't write any message in the text file when input is lower than -273.15.
+
+temperatures = [10,-20,-289,100]
+temp_file = open('temp_file.txt', 'w')
+if def temp (c):
+    if c < -273.15:
+        return "Dont Print"
+    else:
+        f = c * 9/5 + 32
+        return f 
+for t in temperatures:
+    print ("Temp in F is", temp_file.write(temp(t)))
+    temp_file.close()
