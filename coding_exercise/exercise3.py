170b396e565ba40b60b9e8ed7f0ddd966668b0f7
slavo@mimicom24.com <slavo@mimicom24.com>
2017-10-08 22:53:32 -0400

    with statement

--- coding_exercise/exercise3.py
+++ coding_exercise/exercise3.py
@@ -23,3 +23,15 @@ for t in temperatures:
     print ("Temp in F is", temp_file.write(str(temp(t)))
 
 temp_file.close()
+
+# using with statement
+
+temperatures=[10,-20,-289,100]
+ 
+def writer(temperatures):
+    with open("temps.txt", 'w') as file:
+        for c in temperatures:
+            if c>-273.15:
+                f=c*9/5+32
+                file.write(str(f)+"\n")
+writer(temperatures)
