cdb63e70d581bd15db4451134858d133906f8024
slavo@mimicom24.com <slavo@mimicom24.com>
2017-10-08 22:12:21 -0400

    adding read method

--- file_hadling/opening_files.py
+++ file_hadling/opening_files.py
@@ -4,7 +4,7 @@ content = file_obj.read()
 print(content)
 file_obj.close()
 # write in to the file
-first_python_edit = open('new_example.txt', 'w')
+first_python_edit = open('new_example.txt', 'w', 'r')
 first_python_edit.write('Welcome Slavo, You wrote your first line')
 first_python_edit.close()
 print(first_python_edit.read())
