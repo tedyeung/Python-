ff6ac924c9aa305b6d7f70e95cabaee1185da284
Slavoljub Popovic <slavo@mimicom24.com>
2017-10-09 21:58:56 -0400

    import 3-rd party library

---
+++ coding_exercise/exercise4.py
@@ -0,0 +1,6 @@
+# Here is a tricky exercise.Then create a script that merges the three text files into a new text file containing the text of       all three files. The filename of the merged text file should contain the current timestamp down to the millisecond level.   E.g. "2016-06-01-13-57-39-170965.txt".
+# You have some tips in the next lecture and the solution in the lecture after that.
+
+import glob2
+import datetime
+
