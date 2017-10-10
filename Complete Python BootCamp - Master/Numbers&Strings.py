95c0b5144d770690a2d9eec29e1e03bb92da18ef
Slavoljub Popovic <slavo@mimicom24.com>
2017-10-10 18:32:19 -0400

    some basic code

---
+++ Complete Python BootCamp - Master/Numbers&Strings.py
@@ -0,0 +1,129 @@
+
+# coding: utf-8
+
+# Numbers
+
+# In[1]:
+
+
+2 # integer 
+
+
+# In[3]:
+
+
+2.0 # Float 
+
+
+# In[4]:
+
+
+a = 5 # varibale 
+
+
+# Strings
+
+# In[5]:
+
+
+'Hello' # string
+
+
+# In[6]:
+
+
+"Hello" # string with double quotes 
+
+
+# In[7]:
+
+
+print ('Hello World 1')
+print ('Hello World 2')
+
+
+# In[8]:
+
+
+len('Hello World') #lenght of the string 
+
+
+# In[9]:
+
+
+s = 'Hello World'
+
+
+# In[10]:
+
+
+s[0] # indexing 
+
+
+# In[11]:
+
+
+s[3:] # slicesing the strings
+
+
+# In[12]:
+
+
+s[:-1] # backwords 
+
+
+# In[13]:
+
+
+s[::2] 
+
+
+# In[14]:
+
+
+s[::-1] # reversing the string 
+
+
+# In[15]:
+
+
+'immutibility' 
+
+
+# In[17]:
+
+
+"hello world" + " concatenate me"
+
+
+# In[18]:
+
+
+s = 'slavo'
+
+
+# In[19]:
+
+
+s.upper() # method 
+
+
+# In[20]:
+
+
+s.join(' Popovic')
+
+
+# In[27]:
+
+
+name = 'Slavo'
+print (name)
+print ('Hello %s' %(name))
+
+
+# In[30]:
+
+
+print ('Welcome %s, your last name is %s, we are happy to see you here in %s' %('Slavo', 'Popovic', 'Pompano Beach'))
+
