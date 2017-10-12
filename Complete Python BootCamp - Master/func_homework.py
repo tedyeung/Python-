83a04a669e65c2e7df126617c16c45fae7f47a28
Slavoljub Popovic <slavo@mimicom24.com>
2017-10-11 21:04:42 -0400

    sphare func

--- Complete Python BootCamp - Master/func_homework.py
+++ Complete Python BootCamp - Master/func_homework.py
@@ -3,3 +3,5 @@ print ('***** Functions and Methods Homework ******************')
 # Write a function that computes the volume of a sphere given its radius.
 def vol(rad):
     return (4/3)*3,14*(rad**3)
+print('Volume of a sphere is: ',vol(5));
+
