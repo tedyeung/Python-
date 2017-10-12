40a36d02fc8b05a83efdbde824fd54c50a0e0d67
Slavoljub Popovic <slavo@mimicom24.com>
2017-10-11 20:51:39 -0400

    radius

--- Complete Python BootCamp - Master/func_homework.py
+++ Complete Python BootCamp - Master/func_homework.py
@@ -1,2 +1,5 @@
 print ('***** Functions and Methods Homework ******************')
 
+# Write a function that computes the volume of a sphere given its radius.
+def vol(rad):
+    return (4/3)*3,14*(rad**3)
