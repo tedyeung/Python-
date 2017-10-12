03a11e7ee29d471e6a0e4c089f539ad902319977
Slavoljub Popovic <slavo@mimicom24.com>
2017-10-11 21:59:57 -0400

    missing ()

--- Bootcamp/func_homework.py
+++ Bootcamp/func_homework.py
@@ -2,7 +2,7 @@ print ('***** Functions and Methods Homework ******************')
 
 # Write a function that computes the volume of a sphere given its radius.
 def vol(rad):
-    return (4/3)*3,14*(rad**3)
+    return (4/3)*(3,14)*(rad**3)
 print('Volume of a sphere is: ',vol(5));
 print('********************************')
 # Write a function that checks whether a number is in a given range (Inclusive of high and low)
@@ -13,7 +13,7 @@ def ran_bool(num,low,high):
             return True 
        
 print ("Number 3 is in the range (1,10): ", ran_bool(3, 1, 10))
-print ("Number 3 is in the range (1,10): ", ran_bool(2,5,11)
+print ("Number 3 is in the range (1,10): ", ran_bool(2,5,11))
 print ('*************************************************************')
 def ran_check(num,low,high):
     if num in range(low, high+1):
