e42843f4c929f2d3f901d9af9428cc299840d6b0
Slavoljub Popovic <slavo@mimicom24.com>
2017-10-11 21:47:55 -0400

    func

--- Complete Python BootCamp - Master/func_homework.py
+++ Complete Python BootCamp - Master/func_homework.py
@@ -6,4 +6,19 @@ def vol(rad):
 print('Volume of a sphere is: ',vol(5));
 print('********************************')
 # Write a function that checks whether a number is in a given range (Inclusive of high and low)
-
+def ran_bool(num,low,high):
+    num_list = [low for low in range(low, high)]
+    for x in num_list:
+        if num == x:
+            return True 
+       
+print ("Number 3 is in the range (1,10): ", ran_bool(3, 1, 10))
+print ("Number 3 is in the range (1,10): ", ran_bool(2,5,11)
+print ('*************************************************************')
+def ran_check(num,low,high):
+    if num in range(low, high+1):
+        print ('Number %s is in the range' %str(num))
+    else:
+        print ('Number %s is out of the range' %str(num))
+        
+ran_check(3,1,10)
