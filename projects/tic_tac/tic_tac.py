03a30d3cd3fc986dc3ffa163700dbc917fcc79d8
Slavoljub Popovic <slavo@mimicom24.com>
2017-10-15 08:34:49 -0400

    fixing code

--- projects/tic_tac/tic_tac.py
+++ projects/tic_tac/tic_tac.py
@@ -2,9 +2,10 @@
 print ('Wellcome to Tic Tac Toe Game by Slavo7')
 print ('***************************************')
 player_one = input("Please choose, use caplital letters: X or O): ")
-player_two 
+player_two = 'O'
 def ppl():
-    if player_one == 'X':
-        return player_two = 'O'
-def ppl()
-print ('Player one is %s, player two is %s' player_one, player_two)
+    if player_one == 'O':
+         player_two = 'X'
+    return player_two
+ppl()
+print ('Player one is %s, player two is %s' %(player_one, player_two))
