a3cbcb050325b5e13829f64b2d8c8d9d0810c782
Slavoljub Popovic <slavo@mimicom24.com>
2017-10-15 09:00:05 -0400

    assinge value

--- projects/tic_tac/tic_tac.py
+++ projects/tic_tac/tic_tac.py
@@ -2,12 +2,14 @@
 print ('Wellcome to Tic Tac Toe Game by Slavo7')
 print ('***************************************')
 player_one = input("Please choose, use caplital letters: X or O): ")
-player_two = def ppl(player_one):
+def ppl():
     player_two = 'O'
     if player_one == 'O':
          return 'X'
     else:
         return 'O'
+player_two = ppl()
+
 
 
 print ('Player one is %s, player two is %s' %(player_one, player_two))
