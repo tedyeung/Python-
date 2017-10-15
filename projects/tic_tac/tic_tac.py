6dd537a660f6ff175acb1a8e8c0a0495b70cd7fc
Slavoljub Popovic <slavo@mimicom24.com>
2017-10-15 08:26:12 -0400

    set up players

---
+++ projects/tic_tac/tic_tac.py
@@ -0,0 +1,10 @@
+# plyers
+print ('Wellcome to Tic Tac Toe Game by Slavo7')
+print ('***************************************')
+player_one = input("Please choose, use caplital letters: X or O): ")
+player_two 
+def ppl():
+    if player_one == 'X':
+        return player_two = 'O'
+def ppl()
+print ('Player one is %s, player two is %s' player_one, player_two)
