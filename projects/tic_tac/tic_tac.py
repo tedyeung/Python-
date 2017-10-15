62602e0573e262f03e43f26ea3b5db08b8f6fec8
Slavoljub Popovic <slavo@mimicom24.com>
2017-10-15 08:59:07 -0400

    making func

--- projects/tic_tac/tic_tac.py
+++ projects/tic_tac/tic_tac.py
@@ -2,12 +2,12 @@
 print ('Wellcome to Tic Tac Toe Game by Slavo7')
 print ('***************************************')
 player_one = input("Please choose, use caplital letters: X or O): ")
-player_two = None
-def ppl():
+player_two = def ppl(player_one):
+    player_two = 'O'
     if player_one == 'O':
-         player_two = 'X'
-         return player_two
+         return 'X'
     else:
-        return player_two
-ppl()
+        return 'O'
+
+
 print ('Player one is %s, player two is %s' %(player_one, player_two))
