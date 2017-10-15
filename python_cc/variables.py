30072a776243397dd1c77699a764ac69a5f78659
Slavoljub Popovic <slavo@mimicom24.com>
2017-10-15 09:57:02 -0400

    first test

---
+++ python_cc/variables.py
@@ -0,0 +1,10 @@
+# Store and pring varibale
+name = 'Slavo'
+print (neme)
+
+# Sore a variable and then chanege a value of that varibale
+
+company_name = 'MimiCom24'
+print (company_name)
+company_name = 'MCD Security'
+print (company_name)
