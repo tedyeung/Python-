9c2aa21dc5241f9f2187f43a4c7ff07f65ae52ae
Slavoljub Popovic <slavo@mimicom24.com>
2017-10-17 00:46:52 -0400

    print tictac board

--- projects/tic_tac/tic_tac.py
+++ projects/tic_tac/tic_tac.py
@@ -11,9 +11,20 @@ def ppl():
 player_two = ppl()
 print ('Player one is %s, player two is %s' %(player_one, player_two))
 
-board = []
- 
-def myBoard():
-    for num in range(1,4):
-        board.append(num)
-    return board
+def drawBoard(board):
+    # This function prints out the board that it was passed.
+    # "board" is a list of 10 strings representing the board (ignore index 0)
+    print('   |   |')
+    print(' ' + board[7] + ' | ' + board[8] + ' | ' + board[9])
+    print('   |   |')
+    print('-----------')
+    print('   |   |')
+    print(' ' + board[4] + ' | ' + board[5] + ' | ' + board[6])
+    print('   |   |')
+    print('-----------')
+    print('   |   |')
+    print(' ' + board[1] + ' | ' + board[2] + ' | ' + board[3])
+    print('   |   |')
+
+
+drawBoard(board)
