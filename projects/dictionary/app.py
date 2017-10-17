d6e39401b272acbe22f70b6fcd095d2e1fe83c2a
slavo@mimicom24.com <slavo@mimicom24.com>
2017-10-16 22:54:19 -0400

    adding else if statement

--- projects/dictionary/app.py
+++ projects/dictionary/app.py
@@ -4,6 +4,9 @@ data = json.load(open('data.json'))
 
 def import_key ():
     word = input('Please add a word that you are interesting: ')
-    return (data[word])
+    if word in data:
+        return (data[word])
+    else: 
+        print ("This word is out of the range")
 
 print (import_key())
