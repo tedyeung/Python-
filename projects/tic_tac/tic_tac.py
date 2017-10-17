d1c380ba3448280eb0d7ed694a1b2dbf49d10e30
Slavoljub Popovic <slavo@mimicom24.com>
2017-10-17 01:12:50 -0400

    board

--- projects/tic_tac/tic_tac.py
+++ projects/tic_tac/tic_tac.py
@@ -1,16 +1,6 @@
 # plyers
 print ('Wellcome to Tic Tac Toe Game by Slavo7')
 print ('***************************************')
-player_one = input("Please choose, use caplital letters: X or O): ")
-def ppl():
-    player_two = 'O'
-    if player_one == 'O':
-         return 'X'
-    else:
-        return 'O'
-player_two = ppl()
-print ('Player one is %s, player two is %s' %(player_one, player_two))
-
 def drawBoard(board):
     # This function prints out the board that it was passed.
     # "board" is a list of 10 strings representing the board (ignore index 0)
@@ -27,4 +17,5 @@ def drawBoard(board):
     print('   |   |')
 
 
-drawBoard(board)
+
+drawBoard([' ', ' ', ' ', ' ', ' ', ' ', ' ',' ', ' ', ' ' ])
