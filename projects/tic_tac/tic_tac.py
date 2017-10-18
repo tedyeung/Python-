53b7b9dfed972ea0896dd69e956f318fec962444
Slavoljub Popovic <slavo@mimicom24.com>
2017-10-18 19:21:56 -0400

    adding test X to the board

--- projects/tic_tac/tic_tac.py
+++ projects/tic_tac/tic_tac.py
@@ -1,17 +1,14 @@
 print ('WELCOME TO TIC TAC TOE GAME ENJOY')
 print ('**********************************')
 
-board = [" ", " ", " ", " ", " ", " ", " ", " ", " ", " "]
+board = [" ", "X", " ", " ", " ", " ", " ", " ", " ", " "]
 
 def tic_tac_board (board):
     print (' ' + board[1] + '|' + board[2] + '|' + board[3] + " ")
-    print (' ' + " " + '|' + " " + '|' + " " + " ")
     print ("--------")
     print (' ' + board[4] + '|' + board[5] + '|' + board[6] + " ")
-    print (' ' + " " + '|' + " " + '|' + " " + " ")
     print ('--------')
     print (' ' + board[7] + '|' + board[8] + '|' + board[9] + " ")
-    print (' ' + " " + '|' + " " + '|' + " " + " ")
     print ('                                  ')
     print ('**********************************')
 
