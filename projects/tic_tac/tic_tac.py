870c4cfe7fa9662885c82b12357499fcad82cec8
Slavoljub Popovic <slavo@mimicom24.com>
2017-10-18 19:14:34 -0400

    fixing board game

--- projects/tic_tac/tic_tac.py
+++ projects/tic_tac/tic_tac.py
@@ -2,10 +2,12 @@ board = [" ", " ", " ", " ", " ", " ", " ", " ", " ", " "]
 
 def tic_tac_board (board):
     print (' ' + board[1] + '|' + board[2] + '|' + board[3] + " ")
-    print ('___________)
-    print (' ' + board[4] + '|' + board[5] + '|' + board[5] + " ")
+    print (' ' + " " + '|' + " " + '|' + " " + " ")
     print ('___________')
-    print (' ' + board[4] + '|' + board[5] + '|' + board[5] + " ")
+    print (' ' + board[4] + '|' + board[5] + '|' + board[6] + " ")
+    print (' ' + " " + '|' + " " + '|' + " " + " ")
+    print ('___________')
+    print (' ' + board[7] + '|' + board[8] + '|' + board[9] + " ")
 
 
 tic_tac_board(board)
