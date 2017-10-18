bc4888fa8fc923b820d8c7109e88dab2b3f55e14
Slavoljub Popovic <slavo@mimicom24.com>
2017-10-18 19:09:05 -0400

    adding board

--- projects/tic_tac/tic_tac.py
+++ projects/tic_tac/tic_tac.py
@@ -1,21 +1,8 @@
-# plyers
-print ('Wellcome to Tic Tac Toe Game by Slavo7')
-print ('***************************************')
-def drawBoard(board):
-    # This function prints out the board that it was passed.
-    # "board" is a list of 10 strings representing the board (ignore index 0)
-    print('   |   |')
-    print(' ' + board[7] + ' | ' + board[8] + ' | ' + board[9])
-    print('   |   |')
-    print('-----------')
-    print('   |   |')
-    print(' ' + board[4] + ' | ' + board[5] + ' | ' + board[6])
-    print('   |   |')
-    print('-----------')
-    print('   |   |')
-    print(' ' + board[1] + ' | ' + board[2] + ' | ' + board[3])
-    print('   |   |')
+board = [" ", " ", " ", " ", " ", " ", " ", " ", " ", " "]
 
-
-
-drawBoard([' ', ' ', ' ', ' ', ' ', ' ', ' ',' ', ' ', ' ' ])
+def tic_tac_board (board):
+    print (' ' + board[1] + '|' + board[2] + '|' + board[3] + " ")
+    print ('____________________________________________________')
+    print (' ' + board[4] + '|' + board[5] + '|' + board[5] + " ")
+    print ('____________________________________________________')
+    print (' ' + board[4] + '|' + board[5] + '|' + board[5] + " ")
