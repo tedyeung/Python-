50bb7bdcc51e13d0d8e942136446666432e6bbec
Slavoljub Popovic <slavo@mimicom24.com>
2017-10-18 21:12:56 -0400

    adding my board

--- projects/tic_tac/tic_tac.py
+++ projects/tic_tac/tic_tac.py
@@ -2,16 +2,19 @@ print ('WELCOME TO TIC TAC TOE GAME ENJOY')
 print ('**********************************')
 print ('                                  ')
 
-board = [" 0 ", " 1 ", " 2 ", " 3 ", " 4 ", " 5 ", " 6 ", " 7 ", " 8 ", " 9 "]
+board = ["0", "1", "2", "3", "4", "5", "6", "7", "8", "9"]
 
 def tic_tac_board (board):
     print (' ' + board[1] + '|' + board[2] + '|' + board[3] + " ")
-    print ("------------")
+    print ("-------")
     print (' ' + board[4] + '|' + board[5] + '|' + board[6] + " ")
-    print ('------------')
+    print ('-------')
     print (' ' + board[7] + '|' + board[8] + '|' + board[9] + " ")
     print ('                                  ')
     print ('**********************************')
+
+
+tic_tac_board(board)
 
 tic_tac_board(board)
 player_one = 'X'
