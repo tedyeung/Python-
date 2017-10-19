54e4fb795ad62f4921013988a734df595b57e09b
slavo@mimicom24.com <slavo@mimicom24.com>
2017-10-19 19:54:55 -0400

    fixing board

--- projects/tic_solution/app.py
+++ projects/tic_solution/app.py
@@ -2,15 +2,15 @@ from IPython.display import clear_output
 
 def display_board(board):
     print('  | |')
-    print(' ' + board[7] + ' | ' + board[8] + ' | ' + board[9])
+    print(' ' + board[7] + '| ' + board[8] + '| ' + board[9])
     print('  |  |')
     print('-----------')
     print('  |  |')
-    print(' ' + board[4] + ' | ' + board[5] + ' | ' + board[6])
+    print(' ' + board[4] + '| ' + board[5] + '| ' + board[6])
     print('  |  |')
     print('-----------')
     print('  |  |')
-    print(' ' + board[1] + ' | ' + board[2] + ' | ' + board[3])
+    print(' ' + board[1] + '| ' + board[2] + '| ' + board[3])
     print('  |  |')
 
 board = ['X']*10
