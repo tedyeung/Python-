4205a0ca2fc3ce76d5f1d08760028dd29397f8cc
slavo@mimicom24.com <slavo@mimicom24.com>
2017-10-19 19:55:51 -0400

    fixing white spaces

--- projects/tic_solution/app.py
+++ projects/tic_solution/app.py
@@ -1,17 +1,17 @@
 from IPython.display import clear_output
 
 def display_board(board):
-    print('  | |')
-    print(' ' + board[7] + '| ' + board[8] + '| ' + board[9])
-    print('  |  |')
+    print('   | |')
+    print(' ' + board[7] + ' | ' + board[8] + ' | ' + board[9])
+    print('   |  |')
     print('-----------')
-    print('  |  |')
-    print(' ' + board[4] + '| ' + board[5] + '| ' + board[6])
-    print('  |  |')
+    print('   |  |')
+    print(' ' + board[4] + ' | ' + board[5] + ' | ' + board[6])
+    print('   |  |')
     print('-----------')
-    print('  |  |')
-    print(' ' + board[1] + '| ' + board[2] + '| ' + board[3])
-    print('  |  |')
+    print('   |  |')
+    print(' ' + board[1] + ' | ' + board[2] + ' | ' + board[3])
+    print('   |  |')
 
 board = ['X']*10
 display_board(board)
