f1f24a72c15dd5d0888f0d177510b612241b5391
slavo@mimicom24.com <slavo@mimicom24.com>
2017-10-19 20:27:37 -0400

    player input fix

--- projects/tic_solution/app.py
+++ projects/tic_solution/app.py
@@ -15,8 +15,7 @@ display_board(board)
 def player_input():
     marker = ''
     while not (marker == 'O' or marker == "X"):
-        print ('Wrong input please try again!!')
-        marker = input("Player One: Do you want to be X or O").upper()
+        marker = input("Player One: Do you want to be X or O: ").upper()
     if marker == 'X':
         return ('X', 'O')
     else:
