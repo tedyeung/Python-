d42ebc5667c1b49df27b9b93e54fb798d579142d
slavo@mimicom24.com <slavo@mimicom24.com>
2017-10-19 20:01:39 -0400

    writing board

--- projects/tic_solution/app.py
+++ projects/tic_solution/app.py
@@ -1,17 +1,12 @@
 from IPython.display import clear_output
 
 def display_board(board):
-    print('  |  |')
-    print(' ' + board[7] + '|' + board[8] + ' | ' + board[9])
-    print('  |  |')
+    print(' ' + board[7] + ' | ' + board[8] + ' | ' + board[9])
     print('-----------')
-    print('  |  |')
-    print(' ' + board[4] + '|' + board[5] + ' | ' + board[6])
-    print('  |  |')
+    print(' ' + board[4] + ' | ' + board[5] + ' | ' + board[6])
     print('-----------')
-    print('  |  |')
-    print(' ' + board[1] + '|' + board[2] + ' | ' + board[3])
-    print('  |  |')
+    print(' ' + board[1] + ' | '  + board[2] + ' | ' + board[3])
+
 
 board = ['X']*10
 display_board(board)
