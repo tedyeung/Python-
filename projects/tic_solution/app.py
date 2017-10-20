c649699dc5b8367f99900c9c839a4f2af1e96ca3
slavo@mimicom24.com <slavo@mimicom24.com>
2017-10-19 20:47:05 -0400

    full board check

--- projects/tic_solution/app.py
+++ projects/tic_solution/app.py
@@ -44,3 +44,9 @@ def choose_first():
 
 def space_check (board, position):
     return board[position] == ' '
+
+def full_board_check (board):
+    for i in range(1, 10):
+        if space_check(board,i):
+            return False
+    return True
