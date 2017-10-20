50ba40a551146b58c2a81aec2220bf9582b0f073
slavo@mimicom24.com <slavo@mimicom24.com>
2017-10-19 20:37:25 -0400

    check win

--- projects/tic_solution/app.py
+++ projects/tic_solution/app.py
@@ -21,3 +21,16 @@ def player_input():
     else:
         return ('O', 'X')
 player_input()
+
+def place_marker(board, marker, position):
+    board[position] = marker
+
+def win_check(board, mark):
+    return ((board[7] == mark and board[8] == mark and board[9] == mark) or 
+    (board[4] == mark and board[5] == mark and board[6] == mark) or    
+    (board[1] == mark and board[2] == mark and board[3] == mark) or  
+    (board[7] == mark and board[4] == mark and board[1] == mark) or   
+    (board[8] == mark and board[5] == mark and board[2] == mark) or     
+    (board[9] == mark and board[6] == mark and board[3] == mark) or      
+    (board[7] == mark and board[5] == mark and board[3] == mark) or     
+    (board[9] == mark and board[5] == mark and board[1] == mark)     )
