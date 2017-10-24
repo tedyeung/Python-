b8b134f1d18a9e968deeb91c6eae026c4383d7d8
Slavoljub Popovic <slavo@mimicom24.com>
2017-10-24 19:22:10 -0400

    my number

--- python_cc/numbers.py
+++ python_cc/numbers.py
@@ -4,3 +4,7 @@ print ('4/2= ',  4/2)
 print ('4*2= ',  4*2)
 print ('4-2= ',  4-2)
 print ('**************************')
+# example 2
+my_number = 7 
+print ('My favourite number is: ',my_number )
+
