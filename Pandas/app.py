6e3d461f9dd6d1814e4b3b826eaac0cf61a14f39
Slavoljub Popovic <slavo@mimicom24.com>
2017-10-24 18:02:44 -0400

    adding df2

--- Pandas/app.py
+++ Pandas/app.py
@@ -1,6 +1,7 @@
 import pandas
-
 df1 = pandas.DataFrame([[2,4,5], ['slavo', 'nenad', 'pera'], [3,4,5]],columns=['price','name','test'])
-
 print (df1)
+df2 = pandas.DataFrame({"name": "Slavo"},{'job': 'Software Developer'})
+print('***************************************************************')
+print (df2)
 
