667b1a8a63b26c544366d85a835a5a5295208bd4
Slavoljub Popovic <slavo@mimicom24.com>
2017-10-24 19:03:24 -0400

    example 2

--- python_cc/string.py
+++ python_cc/string.py
@@ -1,4 +1,7 @@
 # example 1 
 name = 'Slavo'
 print ('Hello ' + name + ' ,would you like to learn Python today?')
+print ('******************************************************')
+#example 2 upper case, lower case , title case
+print (name.upper(), '\n',  name.lower(), '\n', name.title())
 
