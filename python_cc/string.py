5ee1d7515ed51731a6b14fb4dfa691d20f9fb53e
Slavoljub Popovic <slavo@mimicom24.com>
2017-10-24 19:08:55 -0400

    example 3

--- python_cc/string.py
+++ python_cc/string.py
@@ -2,6 +2,12 @@
 name = 'Slavo'
 print ('Hello ' + name + ' ,would you like to learn Python today?')
 print ('******************************************************')
-#example 2 upper case, lower case , title case
+# example 2 upper case, lower case , title case
 print (name.upper(), '\n',  name.lower(), '\n', name.title())
+print('*****************************************************')
+# Example 3 
+first_name = 'Steve'
+last_name = 'Jobs'
+quote = '"Have the courage to follow your heart and intuition. They somehow know what you truly want to become!"'
+print(fist_name, " ", last_name, 'once said', '\n', quote)
 
