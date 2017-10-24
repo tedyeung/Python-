0b2394d787436d3ca9bfbb2199038ed232a355a4
Slavoljub Popovic <slavo@mimicom24.com>
2017-10-24 17:58:52 -0400

    typo

--- Pandas/app.py
+++ Pandas/app.py
@@ -1,6 +1,6 @@
 import pandas
 
-df1 = pandas.DataFrame([[2,4,5], ['slavo', 'nenad', 'pera'], [3,4,5]])
+df1 = pandas.DataFrame([[2,4,5], ['slavo', 'nenad', 'pera'], [3,4,5]].columns=['price','name','test'])
 
 print (df1)
 
