b3c568780ef882d9b5e58970496649caff259134
Slavoljub Popovic <slavo@mimicom24.com>
2017-10-26 17:07:28 -0400

    using del and remove method

--- python_cc/list_one.py
+++ python_cc/list_one.py
@@ -5,4 +5,8 @@ print('***********************')
 missing_guest = guest.pop()
 print ('Missing guest is: ', missing_guest)
 print (guest)
-
+# using del method
+del guest[3]
+print (guest)
+# using remove method
+guest.remove()
