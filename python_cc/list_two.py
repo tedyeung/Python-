a48be75d19319cfc70f92d3f6a37e3b6156e86dd
Slavoljub Popovic <slavo@mimicom24.com>
2017-10-26 17:25:36 -0400

    using stored method

--- python_cc/list_two.py
+++ python_cc/list_two.py
@@ -1,2 +1,6 @@
 cities = ['Paris', 'Tokio', 'San Francisco', 'New York', 'Belgrade', 'Boston', 'London']
 
+print (cities)
+print ('*************************')
+print (sorted(cities))
+print ('*************************')
