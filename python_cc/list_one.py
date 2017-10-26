cfcacae0dbdb378922e25cd54ea338d5bf9212d5
slavo@mimicom24.com <slavo@mimicom24.com>
2017-10-25 23:59:24 -0400

    new listing

--- python_cc/list_one.py
+++ python_cc/list_one.py
@@ -2,3 +2,7 @@
 guest = ['Marko', 'Nikola', 'Dario', 'Zvezdan', 'Bane', 'Milos']
 print (guest)
 print('***********************')
+missing_guest = guest.pop()
+print ('Missing guest is: ', missing_guest)
+print (guest)
+
