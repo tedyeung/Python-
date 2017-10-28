35b32ef655424379ad5e4a22014be844c666c538
Slavoljub Popovic <slavo@mimicom24.com>
2017-10-28 17:08:04 -0400

    typo

--- python_cc/list_foure.py
+++ python_cc/list_foure.py
@@ -9,7 +9,7 @@ print ('*****************************************')
   #  print (value)
 
 # for min and max loop
-one_m = list(range(1, 100001))
+one_m = list(range(1, 1000001))
 print ('Min number in range from 1 to 1m: ', min(one_m))
 print ('Max number in range from 1 to 1m: ', max(one_m))
 print ('SUm all numbers in range from 1 to 1m: ', sum(one_m))
