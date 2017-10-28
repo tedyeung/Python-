8e1d48e473088b64aeb8b038bbe7ef045f2231c2
Slavoljub Popovic <slavo@mimicom24.com>
2017-10-28 17:49:05 -0400

    wrong slice

--- python_cc/list_five.py
+++ python_cc/list_five.py
@@ -1,5 +1,5 @@
 # slic list 
 print ('Those languages I am willing to learn: ')
 languages  = ['Python', 'R', 'Swift', 'JavaScript', 'Ruby', 'C++', 'C', 'php', 'go', 'Scala', 'Java', 'C#', 'Kotlin']
-for language in languages[0:5]:
+for language in languages[0:4]:
     print (language)
