8d5aa4812f1d13de889d9a468a8bd888694ac5c4
Slavoljub Popovic <slavo@mimicom24.com>
2017-10-28 18:13:51 -0400

    writing touple

---
+++ python_cc/touple.py
@@ -0,0 +1,9 @@
+# writing simpe touple
+languages = ('python', 'r')
+for languge in languages:
+    print(language)
+
+# touple practice
+need_learn = ('python', 'r', 'swift', 'javascript', 'C#')
+for languge in need_learn:
+    print(languge)
