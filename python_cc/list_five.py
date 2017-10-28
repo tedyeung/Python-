5f1eb8cd9dc385a6e780cc574c65565f31ff176a
Slavoljub Popovic <slavo@mimicom24.com>
2017-10-28 17:51:23 -0400

    2nd slice

--- python_cc/list_five.py
+++ python_cc/list_five.py
@@ -3,3 +3,7 @@ print ('Those languages I am willing to learn: ')
 languages  = ['Python', 'R', 'Swift', 'JavaScript', 'Ruby', 'C++', 'C', 'php', 'go', 'Scala', 'Java', 'C#', 'Kotlin']
 for language in languages[0:4]:
     print (language)
+
+print ('Those languages is good to know:')
+for language in languages[5:9]:
+    print(language)
