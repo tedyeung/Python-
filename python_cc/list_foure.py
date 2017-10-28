5fc31686a90adc2f2959796616abdf8a8897aa46
Slavoljub Popovic <slavo@mimicom24.com>
2017-10-28 11:23:00 -0400

    list items

--- python_cc/list_foure.py
+++ python_cc/list_foure.py
@@ -7,3 +7,9 @@ print ('*****************************************')
 # for 1M loop
 for value in range (1, 1000001):
     print (value)
+
+# for min and max loop
+one_m = list(range(1, 100001))
+min(one_m)
+max(one_m)
+sum(one_m)
