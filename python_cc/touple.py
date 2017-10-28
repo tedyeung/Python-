04a4476be5ea5b24a04a91a661ec6a2c85785e76
Slavoljub Popovic <slavo@mimicom24.com>
2017-10-28 18:16:22 -0400

    typo

--- python_cc/touple.py
+++ python_cc/touple.py
@@ -1,9 +1,11 @@
 # writing simpe touple
+print ('My tools for Data Science and Machine Learning: ')
 languages = ('python', 'r')
-for languge in languages:
+for language in languages:
     print(language)
 
 # touple practice
+print('My skills are: ')
 need_learn = ('python', 'r', 'swift', 'javascript', 'C#')
-for languge in need_learn:
-    print(languge)
+for language in need_learn:
+    print(language)
