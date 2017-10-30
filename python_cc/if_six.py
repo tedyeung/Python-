b184500446afa56a6638308feba1da3a1f4e1e9d
Slavoljub Popovic <slavo@mimicom24.com>
2017-10-30 13:45:50 -0400

    fixing variables

--- python_cc/if_six.py
+++ python_cc/if_six.py
@@ -1,10 +1,10 @@
 usernames: ['John', 'Nenad', 'Dragan', 'Marko', 'Jovan', 'Mark', 'Slavo']
 admin = 'Slavo'
-for usernames:
+for usernname in usernames:
     if admin in usernames:
-        print ('Welcome %s, you are admin of this group' %(usernames))
+        print ('Welcome %s, you are admin of this group' %(admin))
     else:
-        print ('Welcome %s, you sign up for Mimicom24' %(usernames))
+        print ('Welcome %s, you sign up for Mimicom24' %(usernname))
 
 print ('Thank You all')
 print ('***********************************')
