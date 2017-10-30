fe9673a5ff2d3972085ddaaa72fa83f3e416718e
Slavoljub Popovic <slavo@mimicom24.com>
2017-10-30 13:41:07 -0400

    print rest

---
+++ python_cc/if_six.py
@@ -0,0 +1,10 @@
+usernames: ['John', 'Nenad', 'Dragan', 'Marko', 'Jovan', 'Mark', 'Slavo']
+
+if usernames:
+    for 'Slavo' in usernames:
+        print ('Welcome %s, you are admin of this group' %(usernames))
+    else:
+        print ('Welcome %s, you sign up for Mimicom24' %(usernames))
+
+print ('Thank You all')
+print ('***********************************')
