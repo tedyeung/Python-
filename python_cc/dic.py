8ea847b37a3baac01917b241c281e087e35d82c3
Slavoljub Popovic <slavo@mimicom24.com>
2017-10-30 18:35:43 -0400

    loop missing ()

--- python_cc/dic.py
+++ python_cc/dic.py
@@ -1,16 +1,21 @@
 # Dictiaries 
 
-user = {
+users = {
     'first_name': 'Slvko',
     'last_name': 'Popovic',
     'age': 35,
     'city': 'Pompano Beach'
     }
 
-print (user['first_name'])
-print (user['last_name'])
-print (user['age'])
-print (user['city'])
-print (user)
+print (users['first_name'])
+print (users['last_name'])
+print (users['age'])
+print (users['city'])
+print (users)
 
-print ('Key: ' + user[key])
+# loop 
+print('************ LOOP ************************************')
+for key, user in users.items:
+    print('\nKey:' + key )
+    print('Value: ' + value)
+
