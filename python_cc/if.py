151a72f21becb807d84c7ff512ec3175f0814749
Slavoljub Popovic <slavo@mimicom24.com>
2017-10-29 23:08:49 -0400

    list

--- python_cc/if.py
+++ python_cc/if.py
@@ -5,3 +5,8 @@ if 'Python' == language:
 
 if 'python' == language.lower():
     print('You can work with ML but please read about R')
+
+languages = ['Python', 'JavaScript', 'Swift', 'php']
+
+if language in languages:
+    print ('Well done you know Python')
