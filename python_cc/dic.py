a496a51dccef71ef35d79cd7237f836aae7ef07e
Slavoljub Popovic <slavo@mimicom24.com>
2017-10-30 18:12:23 -0400

    dicitionry and print

---
+++ python_cc/dic.py
@@ -0,0 +1,14 @@
+# Dictiaries 
+
+user = {
+    'first_name': 'Slvko',
+    'last_name': 'Popovic',
+    'age': 35'
+    'city': 'Pompano Beach'
+    }
+
+print (user['first_name'])
+print (user['last_name'])
+print (user['age'])
+print (user['city'])
+
