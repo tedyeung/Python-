0bb73fd3ae2da42ebbf1bb2c9001b83afadffb54
Slavoljub Popovic <slavo@mimicom24.com>
2017-10-30 14:28:30 -0400

    print both

--- python_cc/if_six.py
+++ python_cc/if_six.py
@@ -1,8 +1,10 @@
-usernames = ['John', 'Nenad', 'Dragan', 'Marko', 'Jovan', 'Mark', 'Slavo']
+usernames = ['Slavo', 'John', 'Nenad', 'Dragan', 'Marko', 'Jovan', 'Mark']
 admins = ['Slavo']
 for username in usernames:
     if username in admins:
+        print('********************************************************')
         print ('Welcome %s, you are admin of this group' %(username))
+        print ('******************************************************')
     else:
         print ('Welcome %s, you sign up for Mimicom24' %(username))
 
