348a167aadf547e21156b26d17f37187b73861d1
Slavoljub Popovic <slavo@mimicom24.com>
2017-10-29 23:21:02 -0400

    green test

--- python_cc/if_one.py
+++ python_cc/if_one.py
@@ -6,3 +6,11 @@ if alien_color == 'red':
 if alien_color == 'green':
     print('Well done you won 5 points')
 print ('****************************************')
+if alien_color == 'red':
+    print ('Well done you won 5 points')
+else:
+    print('You lost try again')
+if alien_color == 'green':
+    print ('Well done you won 5 points')
+else:
+    print('You lost try again')
