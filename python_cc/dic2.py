347fe675d4b0ea197279fbd30a124c6011adb6ed
Slavoljub Popovic <slavo@mimicom24.com>
2017-10-30 18:59:52 -0400

    new dic

--- python_cc/dic2.py
+++ python_cc/dic2.py
@@ -5,5 +5,19 @@ rivers = {
 }
 
 for name, river in rivers.items():
-    print(river.title() + ' is from' + name.title())
+    print(river.title() + ' is from ' + name.title() + '!!')
+
+print ('***************************************************')
+
+favorite_languages = {
+    'jen': 'python', 
+    'sarah': 'c',
+    'edward:' 'ruby',
+    'phil': 'python',
+    'nemanja': 'javaScript',
+    'vlastimir': 'swift',
+    'marko': 'php'
+
+
+}
 
