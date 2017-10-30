3685b632f495b2530bd4d4d22a2aad2146477850
Slavoljub Popovic <slavo@mimicom24.com>
2017-10-30 13:47:01 -0400

    typo ,

--- python_cc/if_six.py
+++ python_cc/if_six.py
@@ -1,10 +1,10 @@
-usernames: ['John', 'Nenad', 'Dragan', 'Marko', 'Jovan', 'Mark', 'Slavo']
+usernames = ['John', 'Nenad', 'Dragan', 'Marko', 'Jovan', 'Mark', 'Slavo']
 admin = 'Slavo'
-for usernname in usernames:
+for username in usernames:
     if admin in usernames:
         print ('Welcome %s, you are admin of this group' %(admin))
     else:
-        print ('Welcome %s, you sign up for Mimicom24' %(usernname))
+        print ('Welcome %s, you sign up for Mimicom24' %(username))
 
 print ('Thank You all')
 print ('***********************************')
