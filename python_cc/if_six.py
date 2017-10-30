ce4b7358a0dd0dd672b4eeeaa92831170b296f49
Slavoljub Popovic <slavo@mimicom24.com>
2017-10-30 17:51:41 -0400

    typo

--- python_cc/if_six.py
+++ python_cc/if_six.py
@@ -20,7 +20,7 @@ print('***************************************')
 numbers = [1,2,3,4,5,6,7,8,9]
 for num in numbers:
     if num == 1:
-        pass:
+        pass
     elif num == 2:
         pass
     elif num == 3:
