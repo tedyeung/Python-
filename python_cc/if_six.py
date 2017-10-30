d566049c6f4e8ba4fa660b487996aaa5e4a4e797
Slavoljub Popovic <slavo@mimicom24.com>
2017-10-30 13:50:12 -0400

    its working now

--- python_cc/if_six.py
+++ python_cc/if_six.py
@@ -1,6 +1,6 @@
 usernames = ['John', 'Nenad', 'Dragan', 'Marko', 'Jovan', 'Mark', 'Slavo']
-admin = 'Slavo'
-for username in usernames:
+admins = ['Slavo']
+for admin in admins:
     if admin in usernames:
         print ('Welcome %s, you are admin of this group' %(admin))
     else:
