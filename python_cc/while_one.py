99a2b260502cbb306d6c07fdc3008b8458104567
Slavoljub Popovic <slavo@mimicom24.com>
2017-10-31 19:12:42 -0400

    dic

--- python_cc/while_one.py
+++ python_cc/while_one.py
@@ -16,7 +16,7 @@ for x in need_to_learn:
 
 print ('\n', '*************************************', '\n')
 
-response = {}
+user_one = {}
 question_one = 'What is you name: '
 question_two = 'What you want to learn: ' 
 
@@ -28,14 +28,16 @@ while status:
     age = input('How old are you? ')
     city = input('Where are you live')
 
-    new_question = input('Do you have any recomendation please answer with y or n: ')
+    user_one["name"] = name.title()
+    user_one["skill"] = skill.title()
+    user_one['age'] = int(age)
+    user_one[city] = city.title()
 
-    response["name"] = name.title()
-    response["skill"] = skill.title()
-    response['age'] = int(age)
-    response[city] = city.title()
+    print (user_one)
+
+    new_question = input('If all info are good say y or type n and \nDo it again: ')
 
     if new_question == 'n':
         status = False
 
-print (response)
+print ('Your info: ', '\n\t', user_one)
