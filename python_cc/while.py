b9bedcdff768ee8b4489aabf1edc2148065089af
Slavoljub Popovic <slavo@mimicom24.com>
2017-10-31 11:52:16 -0400

    while loop

---
+++ python_cc/while.py
@@ -0,0 +1,9 @@
+# while loop
+wishes = []
+question = input('Please enter your wishes: ')
+
+while question != 'Quit':
+    wishes.append(question)
+
+print (wishes)
+print('\n', '****************************', '\n')
