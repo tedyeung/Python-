ab28bbfb2aa641471c2382f5b1afcc8ec7c15181
Slavoljub Popovic <slavo@mimicom24.com>
2017-10-31 13:08:19 -0400

    divider

--- python_cc/while_one.py
+++ python_cc/while_one.py
@@ -2,12 +2,14 @@
 languages = ['Python', 'Swift', 'JavaScript', 'C#', 'R']
 need_to_learn = []
 
+print ('\n', '*************************************', '\n')
+
 while languages:
     lang = languages.pop()
     print('You need to learn %s' %lang)
     need_to_learn.append(lang)
-    
+
 print ('\n', '*************************************', '\n')
 
 for x in need_to_learn:
-    print ("I will learn: ", need_to_learn)
+    print ("I will learn: %s " %x)
