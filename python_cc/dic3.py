45578f67622856a59deb783fd6d598cc1f575711
slavo@mimicom24.com <slavo@mimicom24.com>
2017-10-30 21:56:21 -0400

    printing userlist

---
+++ python_cc/dic3.py
@@ -0,0 +1,32 @@
+user_one = {
+
+    'first_name': 'Slavo',
+    'last_mame': 'Popovic',
+    'age': 35,
+    'language': 'Python'
+} 
+
+user_two = {
+    
+    'first_name': 'Nemanja',
+    'last_mame': 'Stojanovic',
+    'age': 29,
+    'language': 'JavaScript'
+} 
+
+user_tree = {
+    
+    'first_name': 'Dragan',
+    'last_mame': 'Okiljevic',
+    'age': 36,
+    'language': 'Python'
+} 
+
+users_list = [user_one, user_two, user_tree]
+
+for user in users_list:
+    print(users_list)
+print('*************************************')
+
+
+
