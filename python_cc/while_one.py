a955f2085c052ba5e44755a7cd515526bbdbffda
Slavoljub Popovic <slavo@mimicom24.com>
2017-10-31 13:05:55 -0400

    pop method

--- python_cc/while_one.py
+++ python_cc/while_one.py
@@ -3,8 +3,8 @@ languages = ['Python', 'Swift', 'JavaScript', 'C#', 'R']
 need_to_learn = []
 
 while languages:
-    for lang in languages:
-        print('You need to learn %s' %lang)
-        need_to_learn.append(lang)
+    lang = languages.pop()
+    print('You need to learn %s' %lang)
+    need_to_learn.append(lang)
 
 print ("Your list is: ", need_to_learn)
