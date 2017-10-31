e4cc73ae5d7e00315c5fd2b2c526cd3c34b9de3b
Slavoljub Popovic <slavo@mimicom24.com>
2017-10-31 13:15:22 -0400

    loopes

--- python_cc/while_one.py
+++ python_cc/while_one.py
@@ -13,3 +13,24 @@ print ('\n', '*************************************', '\n')
 
 for x in need_to_learn:
     print ("I will learn: %s " %x)
+
+print ('\n', '*************************************', '\n')
+
+user = {}
+question_one = 'What is you name: '
+question_two = 'What you want to learn: ' 
+
+status = True
+
+while status: 
+    name = input(question_one)
+    skill = input(question_two)
+    new_question = input('Do you have any recomendation please answer with y or n: ')
+
+    response[name] = response
+    response[skill] = response
+
+    if new_question == 'n':
+        status = False
+
+print (response)
