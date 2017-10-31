29876a85955c9f6983baa242efd923d41b2fa40e
Slavoljub Popovic <slavo@mimicom24.com>
2017-10-31 11:20:13 -0400

    input and if statement

--- python_cc/input.py
+++ python_cc/input.py
@@ -1,3 +1,15 @@
 # input 
 message = input('What kind of car would you like to rent: ')
 print('Let me take a look if we have that mode ' + message + ' ,this is really nice model!')
+print ('************************************************************************************', '\n ')
+
+# input and if statement , integer 
+
+question = input = ('How many guest are you expecting? ')
+guests = 8
+if int(question) < 8:
+    print('We have a tabel for you, table will have: %s' %question)
+else:
+    print('You have more guest that we can fit let me check if we have table for %s' %question)
+
+print ('************************************************************************************', '\n ')
