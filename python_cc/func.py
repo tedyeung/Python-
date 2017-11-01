7ac53a8c883de6b39ca8e3f3c764d487607032aa
Slavoljub Popovic <slavo@mimicom24.com>
2017-11-01 19:50:02 -0400

    divider

--- python_cc/func.py
+++ python_cc/func.py
@@ -4,14 +4,14 @@ def greet():
 
 greet()
 
-print('************************************************')
+print('\n************************************************\n')
 # simple func with argument
 def book(my_book):
     print ('Slavo please finish all book ' + my_book.title())
 
 book('python crash course')
 
-print('*******************************************************8')
+print('\n*******************************************************\n')
 
 
 
