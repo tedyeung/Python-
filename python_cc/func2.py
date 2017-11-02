ca8f1bc4385e51319ce6019c317dfaa0527efddc
Slavoljub Popovic <slavo@mimicom24.com>
2017-11-02 18:15:47 -0400

    fixing stupid mistake

--- python_cc/func2.py
+++ python_cc/func2.py
@@ -1,11 +1,11 @@
 # simple func with 2 arg
 
 def city_coutry (city, coutry):
-    print(coutry.title(), ' ' , city.title())
+    print(coutry.title(), ',' , city.title())
 
-city_coutry("Belgrade",',', 'Serbia')
-city_coutry('new york',',', 'usa')
-city_coutry('london',',', 'england')
+city_coutry('Belgrade', 'Serbia')
+city_coutry('new york', 'usa')
+city_coutry('london','england')
 
 
     
