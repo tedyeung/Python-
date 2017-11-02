ffedbdd45669182354a30608eb29f5cdea0bf293
Slavoljub Popovic <slavo@mimicom24.com>
2017-11-02 18:13:35 -0400

    adding semi colon

--- python_cc/func2.py
+++ python_cc/func2.py
@@ -3,9 +3,9 @@
 def city_coutry (city, coutry):
     print(coutry.title(), ' ' , city.title())
 
-city_coutry("Belgrade", 'Serbia')
-city_coutry('new york', 'usa')
-city_coutry('london', 'england')
+city_coutry("Belgrade",',', 'Serbia')
+city_coutry('new york',',', 'usa')
+city_coutry('london',',', 'england')
 
 
     
