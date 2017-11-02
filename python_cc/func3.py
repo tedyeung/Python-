c16a6577f55958bb979658d0d8e262bde1c6486a
Slavoljub Popovic <slavo@mimicom24.com>
2017-11-02 19:21:45 -0400

    perfect

--- python_cc/func3.py
+++ python_cc/func3.py
@@ -1,3 +1,7 @@
 # list function
 todo = ['python', 'swift', 'machine learning', 'ios11 and swift4', 'data science', 'deep learning', 'python mega course', '100 python projects']
 
+def todo_list (list_todo):
+    print(list_todo)
+
+todo_list[todo]
