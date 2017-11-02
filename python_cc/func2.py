b65410e8856b87315648975c4348b84ffef38d0e
Slavoljub Popovic <slavo@mimicom24.com>
2017-11-02 18:26:28 -0400

    missing input

--- python_cc/func2.py
+++ python_cc/func2.py
@@ -9,5 +9,22 @@ city_coutry('london','england')
 
 # write a simple func that outpu is dic
 
+def make_album(artist, song):
+    album = {
+        'name': artist,
+        'song': song
+    }
+    return album
 
-    
+active = True
+while active:
+    artist = input('Please add the name of the artist: ')
+    song = input('Plase add a song: ') 
+    question = input('Type N to go out! ')
+    if question != 'N':
+        print('I will ask you again')
+    else:
+        active = False
+        print ('Thank You')
+
+make_album(artist, song)
