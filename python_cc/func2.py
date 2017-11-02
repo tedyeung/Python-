bea2aefc3d6850b52b37f80cf9db97f7ae055382
Slavoljub Popovic <slavo@mimicom24.com>
2017-11-02 18:11:36 -0400

    new project

---
+++ python_cc/func2.py
@@ -0,0 +1,11 @@
+# simple func with 2 arg
+
+def city_coutry (city, coutry):
+    print(coutry.title(), ' ' , city.title())
+
+city_coutry(Belgrade, Serbia)
+city_coutry(new your, usa)
+city_coutry(london, england)
+
+
+    
