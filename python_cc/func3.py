6b433715ace3afb1af72abcc24fed6a3fa6986da
Slavoljub Popovic <slavo@mimicom24.com>
2017-11-02 19:36:44 -0400

    def

--- python_cc/func3.py
+++ python_cc/func3.py
@@ -1,10 +1,13 @@
 # list function
 todo = ['python', 'swift', 'machine learning', 'ios11 and swift4', 'data science', 'deep learning', 'python mega course', '100 python projects']
-
+my_rodo = []
 def todo_list (list_todo):
     print(list_todo)
 
 todo_list(todo)
 
 # new func
-
+def todo_li (todo, todo_list):
+    todos = todo.pop()
+    
+    
