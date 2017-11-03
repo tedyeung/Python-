af94407f870fd181f5d8994781280d41b26f3832
Slavoljub Popovic <slavo@mimicom24.com>
2017-11-02 20:18:43 -0400

    learning curve

--- python_cc/func4.py
+++ python_cc/func4.py
@@ -1,7 +1,8 @@
 # new function 
 def list_todo(*todo):
-    print('You need to learn: ')
-    print(todo.title())
+    print('You need to learn: ', '\t')
+    for todos in todo:
+        print(todos)
 
 list_todo('web site', 'python', 'swift', 'machine learning', 'data science', 'deep learning')
 
