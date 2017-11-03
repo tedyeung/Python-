45b3e4e87a26eaa7ef13594885249ca7f4b1de4d
Slavoljub Popovic <slavo@mimicom24.com>
2017-11-02 20:16:36 -0400

    todo

--- python_cc/func4.py
+++ python_cc/func4.py
@@ -1,7 +1,7 @@
 # new function 
 def list_todo(*todo):
-    for todos in todo:
-        print('You need to learn ', todo.title)
+    print('You need to learn: ')
+    print(todo.title())
 
 list_todo('web site', 'python', 'swift', 'machine learning', 'data science', 'deep learning')
 
