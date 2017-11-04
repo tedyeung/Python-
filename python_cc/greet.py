51ac76d7beea7c2396d7a18da0caa442134a8cc1
Slavoljub Popovic <slavo@mimicom24.com>
2017-11-03 22:14:59 -0400

    adding devider

--- python_cc/greet.py
+++ python_cc/greet.py
@@ -1,4 +1,5 @@
 def greeting (name):
+    print ('\n******************************************************\n')
     print('Welcome %s, you are learning Python for Data Science, Machine Learning, Web Development, Deep Learning, AI and rest :)' %name.title())
 
 
