7d8743271abb068a4d981630e9786d9225604da3
Slavoljub Popovic <slavo@mimicom24.com>
2017-11-04 00:32:15 -0400

    creating user class

--- python_cc/class_one.py
+++ python_cc/class_one.py
@@ -2,3 +2,22 @@
 
 class User():
     
+    def __init__(self, first_name, last_name, age, city):
+        self.first_name = first_name
+        self.last_name = last_name
+        self.age = age
+        self.city = city
+    
+    def greet(slef):
+        print('Welcome %s %s, you registration is ready.' %(self.first_name.title(), self.last_name.title()))
+
+    def skill(self):
+        print('Please enter your bio %s' %self.first_name.tittle())
+
+    def user_info(self):
+        print('Your info \nFull name: %s %s\nAge: %s\nCity: %s' %((self.first_name.title(), self.last_name.title()), str(self.age), self.city.title()))
+
+        
+        
+
+    
