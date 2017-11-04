1dcb86d0ae696bb7d4a7205c9e17c09ca2c540a8
Slavoljub Popovic <slavo@mimicom24.com>
2017-11-04 09:53:35 -0400

    missing c , typo

---
+++ python_cc/class_tree.py
@@ -0,0 +1,30 @@
+# login attempts
+class User():
+    
+    def __init__(self, first_name, last_name, age, city):
+        self.first_name = first_name
+        self.last_name = last_name
+        self.age = age
+        self.city = city
+        self.login = 0
+    
+    def greet(slef):
+        print('Welcome %s %s, you registration is ready.' %(self.first_name.title(), self.last_name.title()))
+
+    def skill(self):
+        print('Please enter your bio %s' %self.first_name.tittle())
+
+    def user_info(self):
+        print('\n*************************************************************\n')
+        print('Your info \nFull name: %s %s\nAge: %s\nCity: %s' %(self.first_name.title(), self.last_name.title(), str(self.age), self.city.title()))
+        print('\n*************************************************************\n')
+
+    def login_att(self, num):
+        login += num
+
+slavo_user = User('slavo', 'popovic', 35, 'fort lauderdale')
+# tamara_user = User('tamara', 'nakic', 33, 'pompano beach')
+# nenad_user = User('nenad', 'sotirovic', 37, 'Dobrec')
+# milos_user = User('milos', 'puric', 36, 'belgrade')
+
+print('Slavo you tried to access: ', slavo_user.login_att(10))
