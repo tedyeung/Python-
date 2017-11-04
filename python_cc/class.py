0208f2c12864355ef2042cbfc7c3ee0c8c3c730e
Slavoljub Popovic <slavo@mimicom24.com>
2017-11-04 00:10:51 -0400

    print

--- python_cc/class.py
+++ python_cc/class.py
@@ -3,3 +3,11 @@ class Restaurant:
     def __init__(self, restauarant_name, restaurant_type):
         self.restauarant_name = name 
         self.restaurant_type = type
+    
+    def describe_restaurant():
+        print ('Welcome to %s, We are %s ' %(name.title(), type.()))
+
+
+restaurant = Restaurant('pasta and...', 'italian restaurante')
+
+print ('Welcome to ', restaurant.name.title())
