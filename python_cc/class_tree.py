62c79173b08935d479194250495e963abee8b5f0
Slavoljub Popovic <slavo@mimicom24.com>
2017-11-04 10:10:34 -0400

    adding while loopt to my func

--- python_cc/class_tree.py
+++ python_cc/class_tree.py
@@ -7,6 +7,7 @@ class User():
         self.age = age
         self.city = city
         self.login = 0
+        self.password = ''
     
     def greet(slef):
         print('Welcome %s %s, you registration is ready.' %(self.first_name.title(), self.last_name.title()))
@@ -18,12 +19,28 @@ class User():
         print('\n*************************************************************\n')
         print('Your info \nFull name: %s %s\nAge: %s\nCity: %s' %(self.first_name.title(), self.last_name.title(), str(self.age), self.city.title()))
         print('\n*************************************************************\n')
-
-    def login_att(self, num):
-        self.login += 10
+    
+    def login_att(self):
+        psw = self.password
+        psw = input("Please add your password: ")
+        active = True
+        while active:
+            if psw != 'mimicom2433062fl':
+                print('Please try again this si your ',  self.login += 1, ' try')
+            else:
+                active = False
+                self.login = 0 
+                print ('Well Done')
+        print ('You have %s' %str(self.login))
+             
+    
+    acitve = True
+    
+    
 
 slavo_user = User('slavo', 'popovic', 35, 'fort lauderdale')
 # tamara_user = User('tamara', 'nakic', 33, 'pompano beach')
 # nenad_user = User('nenad', 'sotirovic', 37, 'Dobrec')
 # milos_user = User('milos', 'puric', 36, 'belgrade')
-print('Slavo you tried to access: ', slavo_user.login_att(10))
+
+slavo_user.login_att()
