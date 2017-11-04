6f80795fdb25072d5cb82ab9041484b43e9adb12
Slavoljub Popovic <slavo@mimicom24.com>
2017-11-03 22:15:53 -0400

    editin func

--- python_cc/greet.py
+++ python_cc/greet.py
@@ -1,6 +1,6 @@
 def greeting (name):
-    print ('\n******************************************************\n')
+    print ('******************************************************\n')
     print('Welcome %s, you are learning Python for Data Science, Machine Learning, Web Development, Deep Learning, AI and rest :)' %name.title())
-
+    print ('\n*****************************************************')
 
     
