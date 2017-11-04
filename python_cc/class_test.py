a22ca8a0f79d098959299b3c8af4f2130c0b0524
Slavoljub Popovic <slavo@mimicom24.com>
2017-11-04 00:53:49 -0400

    typo

--- python_cc/class_test.py
+++ python_cc/class_test.py
@@ -8,13 +8,13 @@ class Dog():
         print(self.name.title() + ' is now sitting!')
 
     def roll_over(self):
-        print(self.name.title + ' rolled over')
+        print(self.name.title() + ' rolled over')
 
 
 
 my_dog = Dog('willie', 6)
 
-print("My dog's name is " + my_dog.name.title )
+print("My dog's name is " + my_dog.name.title() )
 print('My dog is ' + str(my_dog.age) + ' years old')
 
 my_dog.sit()
