9f727319fc58f38b56922f3ad2681b32d6c97218
Slavoljub Popovic <slavo@mimicom24.com>
2017-11-04 09:29:52 -0400

    missing slef arg

--- python_cc/class_two.py
+++ python_cc/class_two.py
@@ -7,9 +7,9 @@ class Restaurant():
         self.place = place
         self.number_served = 0
 
-    def info():
+    def info(self):
         print('Welcome to %s, we are located in %s' %(self.name.title(), self.place.title()))
-        print('We have %s served customers'  str(%self.number_served))
+        print('We have ' + str(self.number_served) + ' customers, we just opened')
 
     def set_number_surved(self, num):
         surved = num + self.number_served
