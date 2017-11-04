a74ae4476cc8c1b6ef949590f8e6abda9251bb11
Slavoljub Popovic <slavo@mimicom24.com>
2017-11-04 09:20:19 -0400

    completing the class

--- python_cc/class_two.py
+++ python_cc/class_two.py
@@ -8,7 +8,11 @@ class Restaurant():
         self.number_served = 0
 
     def set_number_surved(self, num):
-        surved = num + number_served
+        surved = num + self.number_served
         return surved
+ 
+    def increment_surved(self, new_surved):
+        new_surved_num = set_number_surved + new_surved
+        return new_surved_num
 
     
