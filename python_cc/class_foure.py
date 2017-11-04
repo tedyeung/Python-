d8d8b845108639453ca5d000c376c90c7628b77a
Slavoljub Popovic <slavo@mimicom24.com>
2017-11-04 14:17:23 -0400

    adding classes

---
+++ python_cc/class_foure.py
@@ -0,0 +1,27 @@
+class Restaurant():
+    
+    def __init__(self, name, place):
+        self.name = name
+        self.place = place
+        self.number_served = 0
+
+    def info(self):
+        print('Welcome to %s, we are located in %s' %(self.name.title(), self.place.title()))
+        print('We have ' + str(self.number_served) + ' customers, we just opened')
+
+    def set_number_surved(self, num):
+        surved = num + self.number_served
+        return surved
+ 
+    def increment_surved(self, new_surved):
+        new_surved_num = served + new_surved
+        return new_surved_num
+
+# adding class inside the class 
+
+class IceCreamStand(Restaurant):
+    
+    def __init__(self, name, place):
+        super().__init__(self, name, place)
+
+
