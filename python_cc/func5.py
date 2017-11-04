64d5f7df19cc9039e14c30ce3b0c01791f14ffed
Slavoljub Popovic <slavo@mimicom24.com>
2017-11-03 22:13:41 -0400

    import simple func

---
+++ python_cc/func5.py
@@ -0,0 +1,3 @@
+import greet
+
+greet.greeting('slavo')
--- python_cc/greet.py
+++ python_cc/greet.py
@@ -1,5 +1,5 @@
 def greeting (name):
-    print('Welcome %s, you are learning Python for Data Science, Machine Learning, Web Development, Deep Learning, AI and rest :)')
+    print('Welcome %s, you are learning Python for Data Science, Machine Learning, Web Development, Deep Learning, AI and rest :)' %name.title())
 
-    
+
     
