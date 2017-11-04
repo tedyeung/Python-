44cde33e1968c815b56a52ce94982f1566aaeb17
Slavoljub Popovic <slavo@mimicom24.com>
2017-11-04 09:23:11 -0400

    number served

--- python_cc/class_two.py
+++ python_cc/class_two.py
@@ -7,6 +7,10 @@ class Restaurant():
         self.place = place
         self.number_served = 0
 
+    def info():
+        print('Welcome to %s, we are located in %s' %(self.name.title(), self.place.title()))
+        print('We have %s served customers' str(%self.number.served))
+
     def set_number_surved(self, num):
         surved = num + self.number_served
         return surved
@@ -15,4 +19,5 @@ class Restaurant():
         new_surved_num = set_number_surved + new_surved
         return new_surved_num
 
-    
+
+new_restoran = Restaurant('Pasta and ristoranete', 'margate')
