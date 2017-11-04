774d5a899e3cfc46cab05166dce3a0eee5faa359
Slavoljub Popovic <slavo@mimicom24.com>
2017-11-04 14:46:27 -0400

    user info

--- python_cc/class_foure.py
+++ python_cc/class_foure.py
@@ -37,6 +37,57 @@ iceCream = IceCreamStand('pane dolce', 'fort lauderdale')
 iceCream.info_ice_creame()
 
 
+# new class 
+
+class User():
+    
+    def __init__(self, first_name, last_name, age, city):
+        self.first_name = first_name
+        self.last_name = last_name
+        self.age = age
+        self.city = city
+        self.login = 0
+        self.password = ''
+    
+    def greet(slef):
+        print('Welcome %s %s, you registration is ready.' %(self.first_name.title(), self.last_name.title()))
+
+    def skill(self):
+        print('Please enter your bio %s' %self.first_name.tittle())
+
+    def user_info(self):
+        print('\n*************************************************************\n')
+        print('Your info \nFull name: %s %s\nAge: %s\nCity: %s' %(self.first_name.title(), self.last_name.title(), str(self.age), self.city.title()))
+        print('\n*************************************************************\n')
+    
+    def login_att(self):
+        psw = self.password
+        active = True
+        while active:
+            psw = input("Please add your password: ")
+            if psw == 'mimicom2433062fl':
+                active = False
+                self.login = 0 
+                print ('Well Done')
+            else:
+                self.login = self.login + 1
+                print('This is your ', self.login,' try')
+
+        print ('You have %s logs' %str(self.login), '!We did reset of your attempts!')
 
 
+class Admin(User):
 
+    def __init__(self, first_name, last_name, age, city):
+        super __init__(first_name, last_name, age, city)
+        self.privilages = ['can add post', 'can delete post', 'add user', 'remove user']
+
+        def show_privilages(self):
+            print('Admin has those privilages:')
+            for priv in self.privilages:
+                print('/t/t', priv)
+
+
+admin = Administrator('Slavo', 'Popovic', 35, 'pompano beach')
+
+admin.user_info()
