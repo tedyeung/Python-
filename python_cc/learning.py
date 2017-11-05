a2d35ddda40d9a080b3f976c5e12a1f2db24baf8
Slavoljub Popovic <slavo@mimicom24.com>
2017-11-05 09:22:59 -0500

    print list of items

--- python_cc/learning.py
+++ python_cc/learning.py
@@ -3,5 +3,8 @@ file = 'learning_python.txt'
 with open(file) as file_obj:
     lines = file_obj.readlines()
 
+list_items = []
 for line in lines:
-    print(line.rstrip())
+    list_items.append(line[1:])
+
+print (list_items)
