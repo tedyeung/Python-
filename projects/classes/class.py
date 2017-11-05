d8fac65023f14cb81440eac62ff415185c50573e
Slavoljub Popovic <slavo@mimicom24.com>
2017-11-04 23:07:15 -0400

    adding code

--- projects/classes/class.py
+++ projects/classes/class.py
@@ -1,10 +1,23 @@
-class Line(object):
+class Line():
     
     def __init__(self,coor1,coor2):
-        pass
+        self.coor1 = coor1
+        self.coor2 = coor2
     
     def distance(self):
-        pass
+        x1,y1 = self.coor1
+        x2,y2 = self.coor2
+        return ( (x2-x1)**2 + (y2-y1)**2 )**0.5
     
     def slope(self):
-        pass
+        x1,y1 = self.coor1
+        x2,y2 = self.coor2
+        return float((y2-y1))/(x2-x1)
+
+    
+# EXAMPLE OUTPUT
+
+coordinate1 = (3,2)
+coordinate2 = (8,10)
+
+li = Line(coordinate1,coordinate2)
