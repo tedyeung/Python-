4ce4ad63c81826e6ab37a6d035ffd8df5f7d99e6
Slavoljub Popovic <slavo@mimicom24.com>
2017-11-04 20:47:59 -0400

    typo

--- python_cc/user_cl.py
+++ python_cc/user_cl.py
@@ -1,6 +1,6 @@
 import user_cl_two
 
-admin = user_class_two.Admin('Slavo', 'popovic', 35, 'pompano beach')
+admin = user_cl_two.Admin('Slavo', 'popovic', 35, 'pompano beach')
 
 admin.user_info()
 admin.privilages.show_privilage()
