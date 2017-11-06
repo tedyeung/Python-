45d7a96e7d903dd9000b2878cc9a5984f69c3383
Slavoljub Popovic <slavo@mimicom24.com>
2017-11-06 16:51:16 -0500

    missing write method

--- python_cc/write_one.py
+++ python_cc/write_one.py
@@ -8,7 +8,7 @@ while active:
         with open(file_name, 'a') as file_obj:
             file_obj.write('**************************\n')
             file_obj.write('Welcome %s!!!!!' %question.title())
-            file_obj('***********************************')
+            file_obj.write('***********************************')
     else: 
         print ('Thank you, your name is added to write_greeting.txt')
     
