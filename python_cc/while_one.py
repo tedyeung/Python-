b0690aa0a30dbdf9939d7926d4c7e07e5350e2ee
Slavoljub Popovic <slavo@mimicom24.com>
2017-11-06 16:50:36 -0500

    wrong name

---
+++ python_cc/while_one.py
@@ -0,0 +1,43 @@
+# while loop, list 
+languages = ['Python', 'Swift', 'JavaScript', 'C#', 'R']
+need_to_learn = []
+
+print ('\n', '*************************************', '\n')
+
+while languages:
+    lang = languages.pop()
+    print('You need to learn %s' %lang)
+    need_to_learn.append(lang)
+
+print ('\n', '*************************************', '\n')
+
+for x in need_to_learn:
+    print ("I will learn: %s " %x)
+
+print ('\n', '*************************************', '\n')
+
+user_one = {}
+question_one = 'What is you name: '
+question_two = 'What you want to learn: ' 
+
+status = True
+
+while status: 
+    name = input(question_one)
+    skill = input(question_two)
+    age = input('How old are you? ')
+    city = input('Where are you live? ')
+
+    user_one["name"] = name.title()
+    user_one["skill"] = skill.title()
+    user_one['age'] = int(age)
+    user_one['city'] = city.title()
+
+    print (user_one)
+
+    new_question = input('If all info are good say y or type n and \nDo it again: ')
+
+    if new_question == 'y':
+        status = False
+
+print ('Your info: ', '\n\t', user_one)
--- python_cc/write_one.py
+++ python_cc/write_one.py
@@ -1,43 +1,16 @@
-# while loop, list 
-languages = ['Python', 'Swift', 'JavaScript', 'C#', 'R']
-need_to_learn = []
+file_name = 'write_greeting.txt'
 
-print ('\n', '*************************************', '\n')
+active = True
 
-while languages:
-    lang = languages.pop()
-    print('You need to learn %s' %lang)
-    need_to_learn.append(lang)
+while active:
+    question = input('Please eneter your name, if you did please type y: ')
+    if question != 'y':
+        with open(file_name, 'a') as file_obj:
+            file_obj.write('**************************\n')
+            file_obj.write('Welcome %s!!!!!' %question.title())
+            file_obj('***********************************')
+    else: 
+        print ('Thank you, your name is added to write_greeting.txt')
+    
 
-print ('\n', '*************************************', '\n')
-
-for x in need_to_learn:
-    print ("I will learn: %s " %x)
-
-print ('\n', '*************************************', '\n')
-
-user_one = {}
-question_one = 'What is you name: '
-question_two = 'What you want to learn: ' 
-
-status = True
-
-while status: 
-    name = input(question_one)
-    skill = input(question_two)
-    age = input('How old are you? ')
-    city = input('Where are you live? ')
-
-    user_one["name"] = name.title()
-    user_one["skill"] = skill.title()
-    user_one['age'] = int(age)
-    user_one['city'] = city.title()
-
-    print (user_one)
-
-    new_question = input('If all info are good say y or type n and \nDo it again: ')
-
-    if new_question == 'y':
-        status = False
-
-print ('Your info: ', '\n\t', user_one)
+print('Check the file!!!!')
