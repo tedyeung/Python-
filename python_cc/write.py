10c013db58b315380b514d4ac0644162aa0bea8d
Slavoljub Popovic <slavo@mimicom24.com>
2017-11-06 16:34:16 -0500

    adding new file

--- python_cc/write.py
+++ python_cc/write.py
@@ -6,3 +6,4 @@ with open(file_name, 'w') as file_obj:
     file_obj.write('Welcome to Slavo7 Dev File\n')
     file_obj.write('We are here talking about Python, Swift, JavaScript, Mobile Development')
 
+print ('We Just made a write_file.txt')
