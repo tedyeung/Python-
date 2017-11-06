956e9cf2dfdea21b468d1b1036045cd2722681ad
Slavoljub Popovic <slavo@mimicom24.com>
2017-11-06 16:33:11 -0500

    adding simple stuff

---
+++ python_cc/write.py
@@ -0,0 +1,8 @@
+# writing simple line of code 
+
+file_name = 'write_file.txt'
+
+with open(file_name, 'w') as file_obj:
+    file_obj.write('Welcome to Slavo7 Dev File\n')
+    file_obj.write('We are here talking about Python, Swift, JavaScript, Mobile Development')
+
