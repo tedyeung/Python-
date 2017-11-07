dd4518d64835c36b66cca7b1f7ba8721e452c25a
Slavoljub Popovic <slavo@mimicom24.com>
2017-11-07 18:05:24 -0500

    code is working - remeber encoding='utf-8'

--- python_cc/book.py
+++ python_cc/book.py
@@ -3,8 +3,17 @@
 file_name = 'book.txt'
 
 with open(file_name, encoding='utf-8') as file_obj:
-    content = file_obj.read()
-    print (content)
+    content = file_obj.readlines()
+   
 
+
+one_str = ''
+
+for word in content:
+    one_str += word.rstrip()
+
+
+print ('How many word (the) in this book: ')
+print ('Result is: ', one_str.lower().count('the'))
 
 
