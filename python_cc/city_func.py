9e970017d675d64db7093f5255fd26da048c207a
Slavoljub Popovic <slavo@mimicom24.com>
2017-11-08 17:11:49 -0500

    adding ,

--- python_cc/city_func.py
+++ python_cc/city_func.py
@@ -1,6 +1,6 @@
 def city_country (city, country):
-    formated_place = city.title() + ' ' + country.title()
+    formated_place = city.title() + ',' + country.title()
     return formated_place
 
-city_country('belgrade', 'serbia')
+print (city_country('belgrade', 'serbia'))
     
