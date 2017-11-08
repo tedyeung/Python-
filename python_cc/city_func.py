2742b7b4f558e83a292ab21148a0b4bd6d1fa935
Slavoljub Popovic <slavo@mimicom24.com>
2017-11-08 17:10:58 -0500

    fixing arguments

--- python_cc/city_func.py
+++ python_cc/city_func.py
@@ -2,5 +2,5 @@ def city_country (city, country):
     formated_place = city.title() + ' ' + country.title()
     return formated_place
 
-city_country(belgrade, serbia)
+city_country('belgrade', 'serbia')
     
