95848573c35050c669f12047394de855c394239b
Slavoljub Popovic <slavo@mimicom24.com>
2017-11-08 17:38:55 -0500

    adding for test one more output

--- python_cc/test_case/city_func.py
+++ python_cc/test_case/city_func.py
@@ -4,5 +4,6 @@ def city_country (city, country,  population=0):
         formated_place += ' - population ' + str(population)
     return formated_place
 
-print (city_country('belgrade', 'serbia'))
+print (city_country('belgrade', 'serbia', 7000000))
+print (city_country('new york', 'usa', 24000000))
     
