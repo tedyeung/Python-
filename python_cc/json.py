d3882f0e4e57472f2ec3112d374dee82516a7c2b
Slavoljub Popovic <slavo@mimicom24.com>
2017-11-08 06:36:32 -0500

    module 'json' has no attribute dump

--- python_cc/json.py
+++ python_cc/json.py
@@ -7,6 +7,6 @@ user_num = input('Please add your favourite number: ')
 file_name = ('json_file.json')
 
 with open(file_name, 'w') as file_obj:
-    json.dumps(user_num, file_obj)
+    json.dump(user_num, file_obj)
     print('Your favourte number is stored. Number is ', user_num)
 
