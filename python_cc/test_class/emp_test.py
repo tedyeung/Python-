8236d0daa538432067686e8b33e69558f4e54273
Slavoljub Popovic <slavo@mimicom24.com>
2017-11-09 06:45:59 -0500

    testing rising slaray

--- python_cc/test_class/emp_test.py
+++ python_cc/test_class/emp_test.py
@@ -6,4 +6,15 @@ class TestCase(self):
     
     def setUP(self)
     self.slavo = Employee('slavo', 'popovic', 125000)
-    
+
+    def test_give_default_raise(self):
+        self.slavo.give_raise()
+        self.assertEqual(self.slavo.salary, 125000)
+
+    def test_give_raise(self):
+        self.slavo.give_raise(25000)
+        self.assertEqual(self.slavo.salary, 150000)
+
+
+unittest.main()
+        
