77ef4a446ba194db9dd844926e0e78ffb1fef39c
Slavoljub Popovic <slavo@mimicom24.com>
2017-11-18 17:35:23 -0500

    missing [

--- python_cc/data_vizual/plt_scatter.py
+++ python_cc/data_vizual/plt_scatter.py
@@ -5,5 +5,6 @@ power_of_4 = [x**4 for x in (1,10)]
 print ('Value ', value)
 print ('Value power of 4 ', power_of_4)
 plt.scatter(value, power_of_4, s = 20)
-plt.axis(0, 10, 0 , 10000)
+plt.axis([0, 10, 0 , 10000])
+
 plt.show()
