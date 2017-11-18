d7eb2c9116466a1c32992ccaadafdde2e1d375fd
Slavoljub Popovic <slavo@mimicom24.com>
2017-11-18 17:40:33 -0500

    adding code to data vizualization

--- python_cc/data_vizual/plt_scatter.py
+++ python_cc/data_vizual/plt_scatter.py
@@ -4,7 +4,7 @@ value = list(range(1,11))
 power_of_4 = [x**4 for x  in value]
 print ('Value ', value)
 print ('Value power of 4 ', power_of_4)
-plt.scatter(value, power_of_4, s = 20)
-plt.axis([0, 10, 0 , 10000])
 
+plt.scatter(value, power_of_4, s=20)
+plt.axis([0,15,0, 15000])
 plt.show()
