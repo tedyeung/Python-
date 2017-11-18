a720ef710296b3b9e3eae4e24ab8662d4dd1331e
Slavoljub Popovic <slavo@mimicom24.com>
2017-11-18 17:34:22 -0500

    adding code to plt_scatter

---
+++ python_cc/data_vizual/plt_scatter.py
@@ -0,0 +1,9 @@
+import matplotlib
+
+value = list(range(1,11))
+power_of_4 = [x**4 for x in (1,10)]
+print ('Value ', value)
+print ('Value power of 4 ', power_of_4)
+plt.scatter(value, power_of_4, s = 20)
+plt.axis(0, 10, 0 , 10000)
+plt.show()
