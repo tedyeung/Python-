72b02319f04a6d9eeab810a54b57af05cd967845
Slavoljub Popovic <slavo@mimicom24.com>
2017-11-18 07:25:06 -0500

    addding files for practicing

---
+++ python_cc/data_vizual/plt_test.py
@@ -0,0 +1,18 @@
+'''
+A number raised to the third power in cube.Plot the first five cubic numbers. 
+and then plot the first 5000 cubic numbers
+'''
+
+import matplotlib.pyplot as plt
+
+values =[x for x in range(1,6)]
+cubes = [x**3 for x in range(1,6)]
+
+print('Values: ', values)
+print('Cubes: ', cubes)
+plt.title = ('Simple Graph of cube numbers')
+plt.xlabel = ('Values')
+plt.ylable = ('Cubes')
+plt.plot(values, cubes, linewidth=2)
+plt.tick_params(axis='both', labelsize=14)
+plt.show()
