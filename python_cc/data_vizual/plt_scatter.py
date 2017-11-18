1699a886e630a5a464fb7d7db0c78507e135164e
Slavoljub Popovic <slavo@mimicom24.com>
2017-11-18 17:38:59 -0500

    missing in in

--- python_cc/data_vizual/plt_scatter.py
+++ python_cc/data_vizual/plt_scatter.py
@@ -1,7 +1,7 @@
 import matplotlib
 
 value = list(range(1,11))
-power_of_4 = [x**4 for x value]
+power_of_4 = [x**4 for x  in value]
 print ('Value ', value)
 print ('Value power of 4 ', power_of_4)
 plt.scatter(value, power_of_4, s = 20)
