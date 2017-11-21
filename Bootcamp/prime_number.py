ea20f8a0732a0e04637ad44443a18e8703c52a6c
Slavoljub Popovic <slavo@mimicom24.com>
2017-11-21 18:03:20 -0500

    removing break statement

--- Bootcamp/prime_number.py
+++ Bootcamp/prime_number.py
@@ -5,12 +5,14 @@ num_input = int(question)
 
 def prime_number(number):
     if (number % 2 == 0):
-        print (number, ' Your number is not a Prime Number')
-    
-    for i in range(3, (number**0.5) + 1, 2):
-        if (number % 1 == 0):
+        if True:
             print (number, ' Your number is not a Prime Number')
+    
+    for i in range(3, int(number**0.5) + 1, 2):
+        if (number % 1 == 0):
+            if True:
+                print (number, ' Your number is not a Prime Number')
         
-    print (number, ' Your number is a Prime Number')
+    return (number, ' Your number is a Prime Number')
 
 prime_number(num_input)
