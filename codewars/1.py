b618c16f7cb2aabd458bab77196d6012b4de252a
Slavoljub Popovic <slavo@mimicom24.com>
2017-12-02 06:21:13 -0500

    adding problem

---
+++ codewars/1.py
@@ -0,0 +1,17 @@
+'''
+write me a 
+function stringy that 
+takes a size and returns a 
+string of alternating '1s' and '0s'
+'''
+
+a = '1'
+b ='0'
+c = a + b
+print(c)
+def stringy(size):
+    a = '1'
+    b = '0'
+    for i in size:
+        print (a[, b)
+
