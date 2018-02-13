dd9de408dd8b8481f5b66a26bd51b9f8fe30a165
manjabogicevic <slavo@mimicom24.com>
2018-02-13 09:15:35 -0500

    typo wrong name

--- django/first_project/first_app/views.py
+++ django/first_project/first_app/views.py
@@ -1,5 +1,5 @@
 from django.shortcuts import render
-from djago.http import HttpResponse 
+from django.http import HttpResponse 
 
 def index(request):
     return HttpResponse("Slavo your first Django Project")
