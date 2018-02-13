352c39de5276cd71453175f52aafb347ba1f5dff
manjabogicevic <slavo@mimicom24.com>
2018-02-13 09:12:27 -0500

    adding first view to the list

--- django/first_project/first_project/urls.py
+++ django/first_project/first_project/urls.py
@@ -15,7 +15,10 @@ Including another URLconf
 """
 from django.conf.urls import url
 from django.contrib import admin
+from first_app import 
+
 
 urlpatterns = [
+    url(r'^$', views.index.name = 'index'),
     url(r'^admin/', admin.site.urls),
 ]
