4f7012d18816e36d2c0e83410cf13d803b2d3155
manjabogicevic <slavo@mimicom24.com>
2018-02-13 09:14:50 -0500

    typo , had 3rd argument

--- django/first_project/first_project/urls.py
+++ django/first_project/first_project/urls.py
@@ -19,6 +19,6 @@ from first_app import views
 
 
 urlpatterns = [
-    url(r'^$', views.index.name = 'index'),
+    url(r'^$', views.index, name = 'index'),
     url(r'^admin/', admin.site.urls),
 ]
