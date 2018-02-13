edd25d6d782b12faa9f006c250ec95828a5b4897
manjabogicevic <slavo@mimicom24.com>
2018-02-13 18:22:11 -0500

    adding new url

--- django/first_project/first_project/urls.py
+++ django/first_project/first_project/urls.py
@@ -15,10 +15,13 @@ Including another URLconf
 """
 from django.conf.urls import url
 from django.contrib import admin
+from django.conf import include
 from first_app import views
+
 
 
 urlpatterns = [
     url(r'^$', views.index, name = 'index'),
+    url(r'^first_app',include('first_app.urls')),
     url(r'^admin/', admin.site.urls),
 ]
