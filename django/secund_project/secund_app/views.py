e21fe52f4b9deaf687aac8d3eea177f01166b2cd
manjabogicevic <slavo@mimicom24.com>
2018-02-13 09:48:14 -0500

    finished my first problem

--- django/secund_project/secund_app/views.py
+++ django/secund_project/secund_app/views.py
@@ -3,5 +3,5 @@ from django.http import HttpResponse
 
 # Create your views here.
 def index(request):
-    return HttpResponse("Hello to Django World!!")
+    return HttpResponse("<em>Hello to Django World!!</em>")
 
