04f4109b2f3b0d322e867fbe9a286464ded95b72
manjabogicevic <slavo@mimicom24.com>
2018-02-13 18:34:30 -0500

    adding new file

--- django/first_project/first_project/urls.py
+++ django/first_project/first_project/urls.py
@@ -15,7 +15,7 @@ Including another URLconf
 """
 from django.conf.urls import url
 from django.contrib import admin
-from django.conf import include
+from django.conf.urls import include
 from first_app import views
 
 
