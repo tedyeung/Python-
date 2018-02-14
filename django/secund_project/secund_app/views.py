5eafdfc3fd8ea377ecc7559ad3633664823605ef
manjabogicevic <slavo@mimicom24.com>
2018-02-13 21:04:56 -0500

    word wrap

--- django/secund_project/secund_app/views.py
+++ django/secund_project/secund_app/views.py
@@ -3,5 +3,8 @@ from django.http import HttpResponse
 
 # Create your views here.
 def index(request):
-    return HttpResponse("<em>Hello to Django World!!</em>")
+    my_dict = {
+        'text': 'Well Done Slavo!! You did it!!!'
+    }
+    return render(reversed, 'help.html', context=my_dict)
 
