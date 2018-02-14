c0a1e00911d5a0461fc3af4b58f9fac6411eedda
manjabogicevic <slavo@mimicom24.com>
2018-02-13 20:08:07 -0500

    making insert variables

--- django/first_project/first_app/views.py
+++ django/first_project/first_app/views.py
@@ -2,5 +2,5 @@ from django.shortcuts import render
 from django.http import HttpResponse 
 
 def index(request):
-    my_dict = {'inster_me': "Hello I am from views.py!!"}
+    my_dict = { 'insert' : "Hello I am from views.py!!"}
     return render(request, 'index.html', context=my_dict)
