39b25ac3378e249d4ce37e7155bcf3964e2921a2
manjabogicevic <slavo@mimicom24.com>
2018-02-13 20:06:07 -0500

    adding html files

--- django/first_project/first_app/views.py
+++ django/first_project/first_app/views.py
@@ -2,4 +2,5 @@ from django.shortcuts import render
 from django.http import HttpResponse 
 
 def index(request):
-    return HttpResponse("Slavo your first Django Project")
+    my_dict = {'inster_me': "Hello I am from views.py!!"}
+    return render(request, 'index.html', context=my_dict)
