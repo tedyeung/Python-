148da0332d25dd19941ce36ec94d90725da873b2
manjabogicevic <slavo@mimicom24.com>
2018-02-13 21:20:38 -0500

    missing semicolon in the list of items

--- django/secund_project/secund_project/urls.py
+++ django/secund_project/secund_project/urls.py
@@ -20,6 +20,6 @@ from secund_app import views
 
 urlpatterns = [
     url(r'^$', views.index, name='index'),
-    url(r'^help', include('secund_app.urls'))
+    url(r'^help', include('secund_app.urls')),
     url(r'^admin/', admin.site.urls),
 ]
