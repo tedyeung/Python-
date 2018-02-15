8f6f2767a6d014e115f917ae54e7d801caed938d
manjabogicevic <slavo@mimicom24.com>
2018-02-15 18:47:07 -0500

    making models

--- django/first_project/first_app/models.py
+++ django/first_project/first_app/models.py
@@ -1,3 +1,18 @@
 from django.db import models
 
 # Create your models here.
+class Topic(models.Model):
+    top_name = models.CharField(max_length=264, unique=True)
+
+    def __str__(self):
+        return self.top_name
+
+
+class Webpage(models.Model):
+    topic = models.ForeignKey(Topic)
+    name = models.CharField(max_length=264, unique=True)
+    
+
+
+
+  
