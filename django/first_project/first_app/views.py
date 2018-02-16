bc34e7f0cf654df583a1ed236da091a1907f5126
manjabogicevic <slavo@mimicom24.com>
2018-02-16 18:01:13 -0500

    adding webpages_list

--- django/first_project/first_app/views.py
+++ django/first_project/first_app/views.py
@@ -1,6 +1,8 @@
 from django.shortcuts import render
-from django.http import HttpResponse 
+from django.http import HttpResponse
+from first_app.models import Topic,Webpage,AccessRecord
 
 def index(request):
+    webpages_list = AccessRecord.objects.order_by('date')
     my_dict = { 'insert' : "Hello I am from views.py!!"}
     return render(request, 'index.html', context=my_dict)
