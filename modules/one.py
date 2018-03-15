02b9953928704c0fe58ea3ffbad0cb84e5792f71
slavo@mimicom24.com <slavo@mimicom24.com>
2018-03-15 16:27:22 -0400

    adding new package

--- modules/one.py
+++ modules/one.py
@@ -1,7 +1,8 @@
 from termcolor import colored
-
+import pyfiglet as pf 
 # text = colored("Hello Salvo", 'red')
-text = input('What is your name? ')
+# text = input('What is your name? ')
+name = 'Salvo'
 
-
-print(colored(text, 'green', 'on_red'));
+print (pf(name));
+# print(colored(text, 'green', 'on_red'));
