8fdbfadf9fa1354fa66097356802dc0849901c0e
slavo@mimicom24.com <slavo@mimicom24.com>
2018-03-15 16:38:51 -0400

    title

--- modules/one.py
+++ modules/one.py
@@ -2,10 +2,10 @@ from termcolor import colored
 from pyfiglet import Figlet
 
 # text = colored("Hello Salvo", 'red')
-text = input('What is your? ')
-color = input('Please choose one of those colors \n gray, red, green, yellow, blue, magenta, cyan, white')
+text = input('What is your nick? ')
+color = input('Please choose one of those colors \n gray, red, green, yellow, blue, magenta, cyan, white: ')
 f = Figlet(font = 'slant')
 
 
-print (colored(f.renderText(text), color.lower()))
+print (colored(f.renderText(text.title()), color.lower()))
 # print(colored(text, 'green', 'on_red'))
