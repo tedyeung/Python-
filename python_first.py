61bb0fe4c96185d4421978848f81cf0bfce776da
slavo7 <slavo@mimicom24.com>
2018-06-22 22:51:41 -0400

    typo

--- python_first.py
+++ python_first.py
@@ -1,2 +1,9 @@
 print ('Start Learning Python!')
 print ('With Python you can do : Machine Learning,\n Deep Learning,\n Data Science,\n AI, \n Web Development, \n Scripts, \n Desktop APP, \n scripts')
+
+
+def slavo():
+    print ('Hello Slavo')
+
+
+slavo()
