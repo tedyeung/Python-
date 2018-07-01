a7a9b0e9f5c05d65f2c2fb5fc8bca6f79150db7a
slavo@mimicom24.com <slavo@mimicom24.com>
2018-06-30 22:47:05 -0400

    rundom number

--- python3masterclass/game_one.py
+++ python3masterclass/game_one.py
@@ -3,11 +3,13 @@ import random
 print('Hello Slavo')
 
 number = input('Please add number: ')
-
 random_number = random.randint(0,5)
- 
 
+num = int(number)
 
-print(random_number)
-
-print(type(number))
+while random_number:
+    if random_number == num:
+        print('Well Done you found the number')
+        break
+    else: 
+        print('Like number is: ' + random_number)
