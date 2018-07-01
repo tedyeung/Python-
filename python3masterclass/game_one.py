8b48a6e6857fbcc1c33aa6dff2ce4974d55f0271
slavo@mimicom24.com <slavo@mimicom24.com>
2018-06-30 22:50:30 -0400

    adding random number

--- python3masterclass/game_one.py
+++ python3masterclass/game_one.py
@@ -2,12 +2,12 @@ import random
 
 print('Find Number from 0 to 10')
 
-number = input('Please add number: ')
 random_number = random.randint(0,10)
 
 num = int(number)
 
-while random_number:
+while random_number == num:
+    number = input('Please add number: ')
     if random_number == num:
         print('Well Done you found the number')
         break
