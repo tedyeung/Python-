f29283093dce8073d100b9d204f07c5c8300d995
slavo@mimicom24.com <slavo@mimicom24.com>
2018-06-30 22:54:25 -0400

    adding game

--- python3masterclass/game_one.py
+++ python3masterclass/game_one.py
@@ -3,13 +3,13 @@ import random
 print('Find Number from 0 to 10')
 
 random_number = random.randint(0,10)
+game = True
 
-num = int(number)
-
-while random_number == num:
+while game:
     number = input('Please add number: ')
+    num = int(number)
     if random_number == num:
         print('Well Done you found the number')
-        break
+        game = False
     else: 
         print('Like number is: ', random_number)
