167d62c8cee632c7d40f90481bb2891b68f1500a
slavo@mimicom24.com <slavo@mimicom24.com>
2018-08-01 19:37:59 -0400

    fixing quote

--- web-crawler-2018/crawler.py
+++ web-crawler-2018/crawler.py
@@ -5,8 +5,8 @@
 page = '<a href="http://www.mimicom24.com"></a>'
 start_link = page.find('<a href=')
 start_quote = page.find('"', start_link)
-end_quote = page.find('"', start_quote)
-url = page[start_quote + /:end_quote]
+end_quote = page.find('"', start_quote + 1)
+url = page[start_quote + 1:end_quote]
 print (url)
 
 
