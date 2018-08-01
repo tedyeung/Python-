35ba52ba589239c72eba2a51522e8015f6228a6c
slavo@mimicom24.com <slavo@mimicom24.com>
2018-08-01 19:39:15 -0400

    comment

--- web-crawler-2018/crawler.py
+++ web-crawler-2018/crawler.py
@@ -1,7 +1,7 @@
 # Building simple Web Crawler 
 # Using python 3 version
 
-
+# extrating link from web page
 page = '<a href="http://www.mimicom24.com"></a>'
 start_link = page.find('<a href=')
 start_quote = page.find('"', start_link)
