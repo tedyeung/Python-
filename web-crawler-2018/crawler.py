76811277ebadf890f28a4d6e18e3331706afc194
slavo@mimicom24.com <slavo@mimicom24.com>
2018-08-01 19:43:57 -0400

    adding url

--- web-crawler-2018/crawler.py
+++ web-crawler-2018/crawler.py
@@ -3,11 +3,13 @@
 
 # extrating link from web page
 page = '<a href="http://www.mimicom24.com"></a>'
-start_link = page.find('<a href=')
-start_quote = page.find('"', start_link)
-end_quote = page.find('"', start_quote + 1)
-url = page[start_quote + 1:end_quote]
-print (url)
 
+def find_url(page): 
+    start_link = page.find('<a href=')
+    start_quote = page.find('"', start_link)
+    end_quote = page.find('"', start_quote + 1)
+    url = page[start_quote + 1:end_quote]
+    return url
 
+print(find_url(page))
 
