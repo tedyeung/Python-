c857ccfb35470a99ddf90a93e39e1bae0d07ec6c
slavo@mimicom24.com <slavo@mimicom24.com>
2018-08-01 19:34:37 -0400

    string

--- web-crawler-2018/crawler.py
+++ web-crawler-2018/crawler.py
@@ -1,11 +1,13 @@
 # Building simple Web Crawler 
 # Using python 3 version
 
-start_link = page.find('<a href=>')
+
+page = '<a href="http://www.mimicom24.com"></a>'
+start_link = page.find('<a href=')
 start_quote = page.find('"', start_link)
 end_quote = page.find('"', start_quote)
 url = page[start_quote + /:end_quote]
-print url
+print (url)
 
 
 
