05d53f33e38c358407fa5bc34a44537c910ee3b4
slavo@mimicom24.com <slavo@mimicom24.com>
2018-08-01 19:52:30 -0400

    adding argument name

--- web-crawler-2018/crawler.py
+++ web-crawler-2018/crawler.py
@@ -4,11 +4,11 @@
 # extrating link from web page
 page = '<a href="http://www.mimicom24.com"></a>'
 
-def find_url(page): 
-    start_link = page.find('<a href=')
-    start_quote = page.find('"', start_link)
-    end_quote = page.find('"', start_quote + 1)
-    url = page[start_quote + 1:end_quote]
+def find_url(string): 
+    start_link = string.find('<a href=')
+    start_quote = string.find('"', start_link)
+    end_quote = string.find('"', start_quote + 1)
+    url = string[start_quote + 1:end_quote]
     return url
 
 print(find_url(page))
