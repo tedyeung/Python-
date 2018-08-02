d6ce78ece21bb41be995cf90cb89410700692cdd
slavo@mimicom24.com <slavo@mimicom24.com>
2018-08-01 23:00:26 -0400

    removing

--- web-crawler-2018/crawler.py
+++ web-crawler-2018/crawler.py
@@ -36,4 +36,4 @@ test = 'Hello slavo <a href="test1"> welcome <a href="test2"> we are varu welcom
 
 print(all_urls(test))
 
-print(all_urls(page('http://mimicom24.com/')))
+print(all_urls(page('http://mimicom24.com/').encode('utf-8'))))
