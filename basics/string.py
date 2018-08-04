70a8f5c6ed558f9ec5b75eae72c0930adee85e35
slavo@mimicom24.com <slavo@mimicom24.com>
2018-08-03 23:20:44 -0400

    new def

--- basics/string.py
+++ basics/string.py
@@ -6,3 +6,6 @@ print('Hello Slavo\nHello Slavo')
 print ('Hello'.upper().lower())
 
 tableData = [[1,2,3,4],['Slavo','Nemanja', 'Marko', 'Ivan'], ['Python', 'JavaScript', 'Swift','Java']]
+
+def print_table(arr):
+    
