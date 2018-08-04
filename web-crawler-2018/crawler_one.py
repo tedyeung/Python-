3d5627c3e497a2fc54a2b53b5bd159da8126d684
slavo7 <slavo@mimicom24.com>
2018-08-04 12:12:07 -0400

    parsing content

--- web-crawler-2018/crawler_one.py
+++ web-crawler-2018/crawler_one.py
@@ -1,7 +1,10 @@
 # making web crawler using BeautifulSoup
 
 import requests
-from bs4 import BeautifulSoup
+from bs4 import BeautifulSoup as bs4
 
 load_page = requests.get('http://mimicom24.com/')
-type(load_page)
+content_page = load_page.content
+parse_content = bs4(content_page, 'html.parser')
+
+print(parse_content)
