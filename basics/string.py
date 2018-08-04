c3008ab744628d945f3a8ebae5a52e0181e1ac4d
slavo@mimicom24.com <slavo@mimicom24.com>
2018-08-04 10:49:38 -0400

    zip built in function

--- basics/string.py
+++ basics/string.py
@@ -8,4 +8,10 @@ print ('Hello'.upper().lower())
 tableData = [[1,2,3,4],['Slavo','Nemanja', 'Marko', 'Ivan'], ['Python', 'JavaScript', 'Swift','Java']]
 
 def print_table(arr):
+
+for a,b,c in zip(tableData[0], tablData[1], tableData[2]):
+    print (a,b,c)
+
+
     
+
