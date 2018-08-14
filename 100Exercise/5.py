fb89ecb09bf9f6e343d31a01ffb8dd7ca910e100
slavo@mimicom24.com <slavo@mimicom24.com>
2018-08-14 16:47:16 -0400

    printing elements

---
+++ 100Exercise/5.py
@@ -0,0 +1,9 @@
+# Question: Complete the script so that it prints out the second item of the list.
+
+letters = ["a", "b", "c", "d", "e", "f", "g", "h", "i", "j"]
+
+# Expected output: "b"
+
+print(letters[1])
+
+
