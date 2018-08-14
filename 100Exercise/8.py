c0b65ff0dfaf5384085725698ac7a0aef85c9c93
slavo@mimicom24.com <slavo@mimicom24.com>
2018-08-14 16:53:57 -0400

    negative indexing

---
+++ 100Exercise/8.py
@@ -0,0 +1,8 @@
+# Question: Complete the script so that it prints out letter i  using negative indexing.
+
+letters = ["a", "b", "c", "d", "e", "f", "g", "h", "i", "j"]
+
+# Expected output: i
+
+print(letters[-2])
+
