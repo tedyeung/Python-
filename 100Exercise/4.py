7dea6c126e36c1dea1e1fbc95ca8fd9d4ae77736
slavo@mimicom24.com <slavo@mimicom24.com>
2018-08-14 16:45:14 -0400

    coverting to int

---
+++ 100Exercise/4.py
@@ -0,0 +1,7 @@
+# Question: Fix the last line so that it outputs the sum of 1 and 2. Please do not change the first two lines. Only the last one.
+
+a = "1"
+b = 2
+print(int(a) + b)
+# Expected output: 3
+
