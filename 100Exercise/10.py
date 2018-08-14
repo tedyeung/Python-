1987b31a31a91fa7460e807ccbccfcb5ce0a9741
slavo@mimicom24.com <slavo@mimicom24.com>
2018-08-14 17:00:27 -0400

    slicing every secund

---
+++ 100Exercise/10.py
@@ -0,0 +1,6 @@
+# Question: Complete the script so that it prints out a list slice containing letters a, c, e, g, and i. 
+
+letters = ["a", "b", "c", "d", "e", "f", "g", "h", "i", "j"]
+# Expected output: ['a', 'c', 'e', 'g', 'i'] 
+
+print(letters[::2])
