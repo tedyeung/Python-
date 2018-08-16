58efa0b75d8a874eb9b885f23436b68600416979
slavo@mimicom24.com <slavo@mimicom24.com>
2018-08-15 23:05:19 -0400

    new list

--- 100Exercise/12.py
+++ 100Exercise/12.py
@@ -6,4 +6,10 @@ my_range = range(1, 21)
 
 # [10, 20, 30, 40, 50, 60, 70, 80, 90, 100, 110, 120, 130, 140, 150, 160, 170, 180, 190, 200] 
 
-print(list(my_range, 10))
+my_range = list(range(1,21))
+new_list = []
+
+for i in my_range:
+    new_list.append(i*10)
+
+print(new_list)
