38eeef3d7cba6751623f9e290e87f8bf9e0d09a6
slavo@mimicom24.com <slavo@mimicom24.com>
2018-08-17 19:58:05 -0400

    testing filter

--- 100Exercise/21.py
+++ 100Exercise/21.py
@@ -3,3 +3,12 @@
 d = {"a": 1, "b": 2, "c": 3}
 
 # Expected output:  {'a': 1}  
+
+for keys in d:
+    if (d[keys] > 1):
+        print(d[keys]>1)
+    else: 
+        print(False)
+    
+print (d)
+
