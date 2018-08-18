5a191b35b3ff86696a55581a1fdd3507cdf201ea
slavo@mimicom24.com <slavo@mimicom24.com>
2018-08-18 18:56:12 -0400

    python script

---
+++ 100Exercise/36.py
@@ -0,0 +1,11 @@
+'''
+Question: Please download the words1.txt file from the attachment and then create a Python function that takes a text file as input and returns the number of words contained in the text file.
+
+Expected output:
+
+10 
+
+'''
+
+
+
