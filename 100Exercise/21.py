7017dfaac52fd476bf90ad2f13704e7648c5a7bd
slavo@mimicom24.com <slavo@mimicom24.com>
2018-08-17 20:00:34 -0400

    making to dic

--- 100Exercise/21.py
+++ 100Exercise/21.py
@@ -1,14 +1,14 @@
 # Question: Filter the dictionary by removing all items with a value of greater than 1.
 
 d = {"a": 1, "b": 2, "c": 3}
-
+b = {}
 # Expected output:  {'a': 1}  
 
-for keys in d:
-    if (d[keys] > 1):
-        print(d[keys]>1)
+for key in d:
+    if (d[key] > 1):
+        pass
     else: 
-        print(False)
+        b[key] = d[key]
     
-print (d)
+print (b)
 
