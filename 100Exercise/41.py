fd9adc7bdb23331f2e7e964d415bee3efd6a1bc2
slavo@mimicom24.com <slavo@mimicom24.com>
2018-08-18 19:54:33 -0400

    printing in the new line

--- 100Exercise/41.py
+++ 100Exercise/41.py
@@ -2,3 +2,14 @@
 Question: Create a script that generates a text file with all letters of English alphabet inside it, one letter per line.
 '''
 
+from string import ascii_lowercase
+
+alph = ascii_lowercase
+
+def alph_file(data):
+    with open('41.txt', 'w') as f:
+        for a in data:
+            f.write(a + '\n')
+
+alph_file(alph)
+        
