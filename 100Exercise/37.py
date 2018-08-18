6378bc72dea6435bec1046876613a4e331ab2af5
slavo@mimicom24.com <slavo@mimicom24.com>
2018-08-18 19:14:27 -0400

    load file

---
+++ 100Exercise/37.py
@@ -0,0 +1,4 @@
+'''
+Create a function that takes a text file as input and returns the number of words contained in the text file. Please take into consideration that some words can be separated by a comma with no space. For example "Hi,it's me." would need to be counted as three words. For your convenience, you can use the text file in the attachment.
+'''
+
