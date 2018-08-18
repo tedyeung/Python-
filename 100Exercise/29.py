137fbe343c69a0d540e7902a0f5a27b0c67eed3f
slavo@mimicom24.com <slavo@mimicom24.com>
2018-08-18 18:25:18 -0400

    clear

---
+++ 100Exercise/29.py
@@ -0,0 +1,18 @@
+'''
+Exercise 29 - Liquid Volume Calculator
+Section 2, Lecture 62
+
+Question:  Please write a function that calculates liquid volume in a sphere using the following formula. The radius r  is always 10, so consider making it a default parameter.
+
+
+You can then test your solution by passing 2 for h and you should get the expected output.
+
+Expected output:
+
+4071.5040790523717
+'''
+
+from math import pi
+
+p = pi
+print(pi)
