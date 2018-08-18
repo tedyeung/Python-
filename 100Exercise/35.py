35f28099d029c10ffdfe60d03240137397fbee76
slavo@mimicom24.com <slavo@mimicom24.com>
2018-08-18 18:53:33 -0400

    working

--- 100Exercise/35.py
+++ 100Exercise/35.py
@@ -1,3 +1,13 @@
 '''
 Create a function that takes any string as input and returns the number of words for that string.
 '''
+
+some_input = input("How are you? ")
+
+def some_str_number(some_string):
+    split_string = some_string.split(" ")
+    return(len(split_string))
+
+
+
+print(some_str_number(some_input))
