0b420083bb21e2323fd0339bd809f37a82177d73
slavo@mimicom24.com <slavo@mimicom24.com>
2018-08-18 19:47:14 -0400

    new example

--- 100Exercise/40.py
+++ 100Exercise/40.py
@@ -8,3 +8,5 @@ print(math.pow(2))
 import math
 print(math.pow(2,3)) # secund argument is missing
 
+
+
---
+++ 100Exercise/41.py
@@ -0,0 +1,4 @@
+'''
+Question: Create a script that generates a text file with all letters of English alphabet inside it, one letter per line.
+'''
+
