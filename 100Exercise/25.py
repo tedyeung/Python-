0419ffcc7e52d3d0a289a1555fa4b19dacca63d1
slavo@mimicom24.com <slavo@mimicom24.com>
2018-08-18 17:57:05 -0400

    print b

--- 100Exercise/25.py
+++ 100Exercise/25.py
@@ -1,3 +1,18 @@
 # print from a to z all letters 
 
+from string import ascii_letters
+from string import ascii_lowercase
+import re
 
+a = ascii_letters
+b = ascii_lowercase
+
+match = re.search(r"[A-Z]", a)
+
+# a = a.replace(match, '!')
+print(match)
+# print(a)
+print (b)
+for letter in a:
+    if (letter == match):
+        print(letter)
