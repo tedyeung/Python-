27cd282288b6198496f859032fb5dbd502a5b72b
slavo@mimicom24.com <slavo@mimicom24.com>
2018-08-18 16:55:14 -0400

    print dic

--- 100Exercise/22.py
+++ 100Exercise/22.py
@@ -1,1 +1,9 @@
-# Make a dictianlry wich will print a , b , c list of 1 - 30
+# Make a dict wich will print a , b , c list of 1 - 30
+
+d = {
+    "a": list(range(1,11)), 
+    "b": list(range(10,21)),
+    "c": list (range(21,31))
+}
+
+print(d)
