7219d0894fa94717d6834b269a1c5eae74140603
slavo@mimicom24.com <slavo@mimicom24.com>
2018-08-18 16:57:33 -0400

    importing pprint library

--- 100Exercise/22.py
+++ 100Exercise/22.py
@@ -1,9 +1,9 @@
 # Make a dict wich will print a , b , c list of 1 - 30
-
+from pprint import pprint
 d = {
     "a": list(range(1,11)), 
     "b": list(range(10,21)),
     "c": list (range(21,31))
 }
 
-print(d)
+pprint(d)
