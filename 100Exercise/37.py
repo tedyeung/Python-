5a9d27f22e6c5dcbdc382026fc4dd4fdbe5f4184
slavo@mimicom24.com <slavo@mimicom24.com>
2018-08-18 19:18:41 -0400

    replace commas

--- 100Exercise/37.py
+++ 100Exercise/37.py
@@ -2,3 +2,11 @@
 Create a function that takes a text file as input and returns the number of words contained in the text file. Please take into consideration that some words can be separated by a comma with no space. For example "Hi,it's me." would need to be counted as three words. For your convenience, you can use the text file in the attachment.
 '''
 
+def word_count(data):
+    with open(data, 'r') as f:
+        data = f.read()
+
+    data = data.replace(",", " ")
+    return len(data.split(" "))
+
+print(word_count("words2.txt"))
