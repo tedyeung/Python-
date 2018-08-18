6a5040475347e6ff316f2c49f542219f75dedf69
slavo@mimicom24.com <slavo@mimicom24.com>
2018-08-18 18:18:42 -0400

    clear

---
+++ 100Exercise/27.py
@@ -0,0 +1,21 @@
+'''
+Question: 
+
+Create a function that calculates acceleration given initial velocity v1, final velocity v2, start time t1, and end time t2. The formula for acceleration is:
+
+
+To test your solution, call the function by inputting values 0, 10, 0, 20 for v1, v2, t1, and t2 respectively, and you should get the expected output.
+
+Expected output:
+
+0.5
+'''
+
+v1 = 0
+v2 = 10
+t1 = 0
+t2 = 20
+
+def acc (v1,v2,t1,t2):
+    return (v2 - v1)/(t2-t1)
+
