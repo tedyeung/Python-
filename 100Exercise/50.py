aab34b78e8bf9a6f7fcfad7759d6a55a484f1189
slavo@mimicom24.com <slavo@mimicom24.com>
2018-08-19 15:28:09 -0400

    working code

---
+++ 100Exercise/50.py
@@ -0,0 +1,12 @@
+'''
+Question: The code produces an error. Please understand the error and try to fix it
+
+age = input("What's your age? ")
+age_last_year = age - 1
+print("Last year you were %s." % age_last_year)
+Note: Please use raw_input instead of input if you are on Python 2. For Python 3 input is fine.
+'''
+
+age = input("What's your age? ")
+age_last_year = int(age) - 1
+print("Last year you were %s." % age_last_year)
