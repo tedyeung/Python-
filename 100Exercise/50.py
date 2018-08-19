30eae27fa4b726cb7c5f8e9dee8a7f8f8b48d0fa
slavo@mimicom24.com <slavo@mimicom24.com>
2018-08-19 15:32:45 -0400

    convert to int

--- 100Exercise/50.py
+++ 100Exercise/50.py
@@ -7,6 +7,6 @@ print("Last year you were %s." % age_last_year)
 Note: Please use raw_input instead of input if you are on Python 2. For Python 3 input is fine.
 '''
 
-age = input("What's your age? ")
+age = input("What's your age? ") # string - needs to convert to age
 age_last_year = int(age) - 1
 print("Last year you were %s." % age_last_year)
