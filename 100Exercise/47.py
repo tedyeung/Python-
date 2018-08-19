aa434d9391e308fb4ea04b2f493a09071f0ce6d4
slavo@mimicom24.com <slavo@mimicom24.com>
2018-08-19 15:12:53 -0400

    working

---
+++ 100Exercise/47.py
@@ -0,0 +1,17 @@
+# check if letters inside the text file blongs to pyhton word
+
+from string import ascii_lowercase
+
+ascii_string = ascii_lowercase
+name = 'python'
+python_letter_list = []
+
+for char in ascii_string:
+    for letter in name:
+        with open("46" + '/' + char + ".txt", 'r') as f:
+            data = f.read()
+        if(data == letter):
+            python_letter_list.append(letter)
+
+
+print('Result: ',python_letter_list)
