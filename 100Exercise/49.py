5870241e0d11a3798a8828d3fbeb64e8cb78652c
slavo@mimicom24.com <slavo@mimicom24.com>
2018-08-19 15:26:37 -0400

    python

---
+++ 100Exercise/49.py
@@ -0,0 +1,14 @@
+'''
+
+Question: The code is supposed to get some input from the user, but instead it produces an error. Please try to understand the error and then fix it.
+
+pass = input("Please enter your password: ") 
+
+Note: Please use raw_input  instead of input if you are on Python 2. For Python 3 input is fine.
+
+'''
+
+password = input("Please enter your password: ") 
+print(password)
+
+# pass is a keyword
