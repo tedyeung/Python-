70a83af3014918063cd45df51f43494280d70168
slavo@mimicom24.com <slavo@mimicom24.com>
2018-08-18 20:02:16 -0400

    clear

---
+++ 100Exercise/42.py
@@ -0,0 +1,14 @@
+# Question: Print out in each line the sum of homologous items of the two sequences.
+
+a = [1, 2, 3]
+b = (4, 5, 6)
+
+# Expected output: 
+'''
+5
+7
+9
+'''
+
+for x,y in zip(a,b):
+    print (x + y) 
