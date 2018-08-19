dea48ede9c9ab18c4ee79876b833682dbbd58d77
slavo@mimicom24.com <slavo@mimicom24.com>
2018-08-18 20:23:35 -0400

    new line of code

---
+++ 100Exercise/44.py
@@ -0,0 +1,9 @@
+'''
+Question: Create a script that generates a file where all letters of English alphabet are listed three in each line. The inside of the text file would look like:
+
+abc
+def
+ghi
+
+and so on.
+'''
