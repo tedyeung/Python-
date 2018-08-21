19b6fb6498da7336c2ad741f194f79748dd3542e
slavo@mimicom24.com <slavo@mimicom24.com>
2018-08-20 21:23:46 -0400

    json dumps

---
+++ 100Exercise/56.py
@@ -0,0 +1,21 @@
+'''
+Question: Store the dictionary in a json file.
+
+d = {"employees":[{"firstName": "John", "lastName": "Doe"},
+                {"firstName": "Anna", "lastName": "Smith"},
+                {"firstName": "Peter", "lastName": "Jones"}],
+"owners":[{"firstName": "Jack", "lastName": "Petter"},
+          {"firstName": "Jessy", "lastName": "Petter"}]}
+'''
+
+
+d = {"employees":[{"firstName": "John", "lastName": "Doe"},
+                {"firstName": "Anna", "lastName": "Smith"},
+                {"firstName": "Peter", "lastName": "Jones"}],
+"owners":[{"firstName": "Jack", "lastName": "Petter"},
+          {"firstName": "Jessy", "lastName": "Petter"}]}
+
+import json
+
+d = json.dumps(d)
+print (d)
