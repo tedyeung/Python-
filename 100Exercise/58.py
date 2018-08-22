a8761e951cad6d8506ea19059912940d12a16982
slavo@mimicom24.com <slavo@mimicom24.com>
2018-08-22 19:53:06 -0400

    loading json file

--- 100Exercise/58.py
+++ 100Exercise/58.py
@@ -8,4 +8,9 @@ from pprint import pprint as pp
 with open("company1.json") as json_file:
     data = json.load(json_file)
 
+data["employees"].append({"firstName": "Slavo", "lastName": "Popovic"})
+
 pp(data)
+
+with open("company1.json", "w") as json_file:
+    json.dump(data, json_file)
